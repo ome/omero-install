ome/openmicroscopy

Remove RELEASE-VERSION from docs/install/VM (Fix #9637)
The "RELEASE" target type has been untested for sometime.
Instead the "QA" type has always been built and then renamed
on release. For the moment, we'll simplify the configuration
and only use "QA" builds.

docs/install/VM/setup_omero.sh
@@ -3,7 +3,6 @@
 set -e -u -x
 
 PGPASSWORD=${PGPASSWORD:-"omero"}
-TARGET=${TARGET:-"QA"} # NB. Valid args are {QA | RELEASE}
 URL_RELEASE="http://cvs.openmicroscopy.org.uk/snapshots/omero/"
 RELEASE_ARCHIVE="OMERO.server-Beta-4.3.4.zip"
 RELEASE_FOLDER=${RELEASE_ARCHIVE%.zip}
@@ -13,32 +12,25 @@ OMERO_PATH="/home/omero/OMERO.server"
 OMERO_BIN=$OMERO_PATH/bin
 INSTALL_FOLDER="OMERO.server"
 
-if [[ "$TARGET" == "RELEASE" ]]; then
-	echo "Grabbing current stable release of OMERO.server: $RELEASE_ARCHIVE"
-	wget -q $URL_RELEASE$RELEASE_ARCHIVE
-	unzip $RELEASE_ARCHIVE
-	mv $RELEASE_FOLDER $INSTALL_FOLDER
+echo "Grabbing last successful QA Build of OMERO.server"
+DL_ARCHIVE=""
+if [ "x$DL_ARCHIVE" == "x" ]; then
+
+    URL=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-stable/lastSuccessfulBuild/api/xml?xpath=/freeStyleBuild/url/text()"`
+    FILE=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-stable/lastSuccessfulBuild/api/xml?xpath=//relativePath[contains(.,'server')]/text()"`
+
+    wget -q "$URL"artifact/$FILE
+
+    DL_ARCHIVE=`basename $FILE`
+    DL_FOLDER=${DL_ARCHIVE%.zip}
 else
-	echo "Grabbing last successful QA Build of OMERO.server"
-    DL_ARCHIVE=""
-    if [ "x$DL_ARCHIVE" == "x" ]; then
+    DL_LOC="http://hudson.openmicroscopy.org.uk/job/OMERO-stable/lastSuccessfulBuild/artifact/"
+    DL_FOLDER=${DL_ARCHIVE%.zip}
 
-    	URL=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-stable/lastSuccessfulBuild/api/xml?xpath=/freeStyleBuild/url/text()"`
-    	FILE=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-stable/lastSuccessfulBuild/api/xml?xpath=//relativePath[contains(.,'server')]/text()"`
-
-    	wget -q "$URL"artifact/$FILE
-
-        DL_ARCHIVE=`basename $FILE`
-    	DL_FOLDER=${DL_ARCHIVE%.zip}
-    else
-        DL_LOC="http://hudson.openmicroscopy.org.uk/job/OMERO-stable/lastSuccessfulBuild/artifact/"
-        DL_FOLDER=${DL_ARCHIVE%.zip}
-    
-    	wget $DL_LOC$DL_ARCHIVE
-    fi
-    unzip $DL_ARCHIVE
-    mv $DL_FOLDER $INSTALL_FOLDER
+    wget $DL_LOC$DL_ARCHIVE
 fi
+unzip $DL_ARCHIVE
+mv $DL_FOLDER $INSTALL_FOLDER
 
 mkdir OMERO.data
 

ome/omero-install

setup_omero.sh
@@ -3,7 +3,6 @@
 set -e -u -x
 
 PGPASSWORD=${PGPASSWORD:-"omero"}
-TARGET=${TARGET:-"QA"} # NB. Valid args are {QA | RELEASE}
 URL_RELEASE="http://cvs.openmicroscopy.org.uk/snapshots/omero/"
 RELEASE_ARCHIVE="OMERO.server-Beta-4.3.4.zip"
 RELEASE_FOLDER=${RELEASE_ARCHIVE%.zip}
@@ -13,32 +12,26 @@ OMERO_PATH="/home/omero/OMERO.server"
 OMERO_BIN=$OMERO_PATH/bin
 INSTALL_FOLDER="OMERO.server"
 
-if [[ "$TARGET" == "RELEASE" ]]; then
-	echo "Grabbing current stable release of OMERO.server: $RELEASE_ARCHIVE"
-	wget -q $URL_RELEASE$RELEASE_ARCHIVE
-	unzip $RELEASE_ARCHIVE
-	mv $RELEASE_FOLDER $INSTALL_FOLDER
+echo "Grabbing last successful QA Build of OMERO.server"
+DL_ARCHIVE=""
+if [ "x$DL_ARCHIVE" == "x" ]; then
+
+    URL=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-trunk/lastSuccessfulBuild/api/xml?xpath=/freeStyleBuild/url/text()"`
+    FILE=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-stable/lastSuccessfulBuild/api/xml?xpath=//relativePath[contains(.,'server')]/text()"`
+
+    wget -q "$URL"artifact/$FILE
+
+    DL_ARCHIVE=`basename $FILE`
+    DL_FOLDER=${DL_ARCHIVE%.zip}
 else
-	echo "Grabbing last successful QA Build of OMERO.server"
-    DL_ARCHIVE=""
-    if [ "x$DL_ARCHIVE" == "x" ]; then
 
-    	URL=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-trunk/lastSuccessfulBuild/api/xml?xpath=/freeStyleBuild/url/text()"`
-    	FILE=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-trunk/lastSuccessfulBuild/api/xml?xpath=//relativePath[contains(.,'server')]/text()"`
+    DL_LOC="http://hudson.openmicroscopy.org.uk/job/OMERO-trunk/lastSuccessfulBuild/artifact/"
+    DL_FOLDER=${DL_ARCHIVE%.zip}
 
-    	wget -q "$URL"artifact/$FILE
-
-        DL_ARCHIVE=`basename $FILE`
-    	DL_FOLDER=${DL_ARCHIVE%.zip}
-    else
-        DL_LOC="http://hudson.openmicroscopy.org.uk/job/OMERO-trunk/lastSuccessfulBuild/artifact/"
-        DL_FOLDER=${DL_ARCHIVE%.zip}
-    
-    	wget $DL_LOC$DL_ARCHIVE
-    fi
-    unzip $DL_ARCHIVE
-    mv $DL_FOLDER $INSTALL_FOLDER
+    wget $DL_LOC$DL_ARCHIVE
 fi
+unzip $DL_ARCHIVE
+mv $DL_FOLDER $INSTALL_FOLDER
 
 mkdir OMERO.data
 