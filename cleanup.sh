299d19ad2d906d018104c5c90041b703ae84d005
Simon Li <spli@dundee.ac.uk>
2014-05-13 17:16:44 +0100

    Copied docs/install/VM from omerovm_update into a new repo Removed VM specific files
    git subtree split --prefix=docs/install/VM -b split_vm
    git push ../omero-install/ split_vm

--- cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e -u -x
-
-sudo userdel -f -r vagrant
-sudo rm -f /etc/sudoers.d/vagrant
--- setup_environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -u -x
-
-chmod a+rx $HOME
-echo "export OMERO_PREFIX=$OMERO_PREFIX" >> ~/.bashrc
-echo 'export PATH=$PATH:$OMERO_PREFIX/bin' >> ~/.bashrc
-
-(source ~/.bashrc) || true
