4ec0b27da5b20092d6145bcb42441cceb9ee42f5
Simon Li <spli@dundee.ac.uk>
2014-05-13 19:10:59 +0100

    Add missing system_setup.sh
    Comment out methods for starting omero

--- install-debian7-ubuntu1404.sh
+++ install-debian7-ubuntu1404.sh
@@ -1,6 +1,7 @@
 dependencies-debian7-ubuntu1404.sh
-settings.env
+source settings.env
 
+system_setup.sh
 setup_postgres.sh
 
 cd ~omero
@@ -8,8 +9,11 @@ sudo -iu omero bash setup_omero.sh
 
 setup_nginx.sh
 
-sudo -iu omero OMERO.server/bin/omero admin start
-sudo -iu omero OMERO.server/bin/omero web start
+#sudo -iu omero OMERO.server/bin/omero admin start
+#sudo -iu omero OMERO.server/bin/omero web start
 
 setup_omero_daemon.sh
+
+#service omero start
+#service omero-web start
 
