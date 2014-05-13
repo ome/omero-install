466d493a0d9a53df5ea6325340e17be018813d21
Simon Li <orpheus+devel@gmail.com>
2014-05-14 00:44:43 +0100

    Missing backslashes

--- dependencies-debian7-ubuntu1404.sh
+++ dependencies-debian7-ubuntu1404.sh
@@ -1,6 +1,6 @@
 apt-get -y install \
-    unzip
-    wget
+    unzip \
+    wget \
     python-{imaging,matplotlib,numpy,pip,scipy,tables,virtualenv} \
     openjdk-7-jre-headless \
     zeroc-ice \
