a0682ecba7e4a3374dd2db98451f41d70ea0e430
Simon Li <spli@dundee.ac.uk>
2014-05-15 00:56:02 +0100

    Make unzip quieter

--- setup_omero_ice35.sh
+++ setup_omero_ice35.sh
@@ -7,7 +7,7 @@ source settings.env
 SERVER=http://downloads.openmicroscopy.org/omero/5.0.1/artifacts/OMERO.server-5.0.1-ice35-b21.zip
 
 wget $SERVER
-unzip `basename $SERVER`
+unzip -q `basename $SERVER`
 ln -s `basename "${SERVER%.zip}"` OMERO.server
 
 OMERO.server/bin/omero config set omero.data.dir "$OMERO_DATA_DIR"
