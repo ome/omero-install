3b3a1c6bce5e7babf769acc45b236c5e38df5207
Simon Li <spli@dundee.ac.uk>
2014-05-14 22:17:17 +0100

    Disable pager in psql list

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -4,5 +4,5 @@ echo "CREATE USER $OMERO_DB_USER PASSWORD '$OMERO_DB_PASS'" | \
     sudo -u postgres psql
 sudo -u postgres createdb -O "$OMERO_DB_USER" "$OMERO_DB_NAME"
 
-psql -h localhost -U "$OMERO_DB_USER" -l
+psql -P pager=off -h localhost -U "$OMERO_DB_USER" -l
 
