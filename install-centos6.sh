7252e55c7e0cfbbe5b6114f32b95d37b58bcffaf
Simon Li <spli@dundee.ac.uk>
2014-05-14 22:35:27 +0100

    Fix setup_nginx.sh call for centos6

--- install-centos6.sh
+++ install-centos6.sh
@@ -2,6 +2,9 @@
 
 set -e -u -x
 
+# Full path of the current directory containing the install scripts
+INSTALL_SCRIPTS=$(readlink -f $(dirname $0))
+
 bash -eux dependencies-centos6.sh
 source settings.env
 
@@ -11,8 +14,9 @@ bash -eux setup_postgres.sh
 cp settings.env setup_omero_ice35.sh ~omero
 sudo -iu omero bash -eux setup_omero_ice35.sh
 
-cd ~omero
-bash -eux setup_nginx.sh
+pushd ~omero
+bash -eux $INSTALL_SCRIPTS/setup_nginx.sh
+popd
 
 #sudo -iu omero OMERO.server/bin/omero admin start
 #sudo -iu omero OMERO.server/bin/omero web start
