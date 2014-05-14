62eab8aec5ca1d1006071424ab638b02bef6b0b3
Simon Li <spli@dundee.ac.uk>
2014-05-14 23:30:47 +0100

    sudo is needed for the Debian7 Docker image

--- dependencies-debian7-ubuntu1404.sh
+++ dependencies-debian7-ubuntu1404.sh
@@ -4,6 +4,7 @@ apt-get update
 apt-get -y install \
 	unzip \
 	wget \
+	sudo \
 	python-{imaging,matplotlib,numpy,pip,scipy,tables,virtualenv} \
 	openjdk-7-jre-headless \
 	ice-services python-zeroc-ice \
