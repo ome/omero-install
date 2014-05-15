a9e603bf002d292a5e3ab2a2f8f686a61041f5d2
Simon Li <spli@dundee.ac.uk>
2014-05-15 12:15:36 +0100

    Create a local zip instead of downloading a remote repo

---
+++ test/docker-build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+	echo "USAGE: `basename $0` distribution"
+	exit 2
+fi
+
+set -e
+
+rm -rf omero-install-test
+mkdir omero-install-test
+cp ../*.sh ../*.env ../*init.d omero-install-test
+zip -r $1/omero-install-test.zip omero-install-test
+rm -rf omero-install-test
+
+IMAGE=omero-install-test-$1
+echo "Building image $IMAGE"
+docker build -t $IMAGE --no-cache $1
+echo "Test this image by running docker run -it [...] $IMAGE"
+
