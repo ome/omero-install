6ca41e4d77f57eaad155a4686ea68c5325a7daca
Simon Li <spli@dundee.ac.uk>
2014-05-21 15:37:53 +0100

    Forgot to split the contents of setup_omero_daemon*

--- setup_omero_daemon_ubuntu1404.sh
+++ setup_omero_daemon_ubuntu1404.sh
@@ -6,19 +6,7 @@ chmod a+x /etc/init.d/omero
 cp omero-web-init.d /etc/init.d/omero-web
 chmod a+x /etc/init.d/omero-web
 
-if [ -f /usr/sbin/update-rc.d ]; then
-    # Debian
-    update-rc.d -f omero remove
-    update-rc.d -f omero defaults 98 02
-    update-rc.d -f omero-web remove
-    update-rc.d -f omero-web defaults 98 02
-elif [ -f /sbin/chkconfig ]; then
-    # Redhat
-    chkconfig --del omero
-    chkconfig --add omero
-    chkconfig --del omero-web
-    chkconfig --add omero-web
-else
-    echo "ERROR: Failed to find init.d management script"
-    exit 2
-fi
+update-rc.d -f omero remove
+update-rc.d -f omero defaults 98 02
+update-rc.d -f omero-web remove
+update-rc.d -f omero-web defaults 98 02
