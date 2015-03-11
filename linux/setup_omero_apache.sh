3881f20692de975f628111c440727d035b39cba9
Simon Li <spli@dundee.ac.uk>
2015-03-11 17:01:44 +0000

    Remove omero.web.application_server fastcgi-tcp (default)

--- linux/setup_omero_apache.sh
+++ linux/setup_omero_apache.sh
@@ -4,5 +4,4 @@ set -e -u -x
 
 source settings.env
 
-OMERO.server/bin/omero config set omero.web.application_server fastcgi-tcp
 OMERO.server/bin/omero web config apache --system --http "$OMERO_WEB_PORT" > OMERO.server/apache.conf.tmp
