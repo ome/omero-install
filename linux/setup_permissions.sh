15428a27a4143057c59cbaf2deb90e81d57770f4
Simon Li <spli@dundee.ac.uk>
2015-05-21 10:24:23 +0100

    Add note about protecting OMERO data dir

--- linux/setup_permissions.sh
+++ linux/setup_permissions.sh
@@ -5,5 +5,7 @@ set -e -u -x
 source settings.env
 
 chmod go-rwx ~omero/OMERO.server/etc ~omero/OMERO.server/var
+
+# Optionally restrict accesss to the OMERO data directory
 #chmod go-rwx "$OMERO_DATA_DIR"
 
