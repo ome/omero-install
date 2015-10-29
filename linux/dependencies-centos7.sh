cd4d63c96c43c06dc3b93f843f0fe96b7ba38aab
Simon Li <spli@dundee.ac.uk>
2015-10-29 12:12:33 +0000

    Use java 1.8, epel pytables, postgres 9.4

--- linux/dependencies-centos7.sh
+++ linux/dependencies-centos7.sh
@@ -8,22 +8,16 @@ curl -o /etc/yum.repos.d/zeroc-ice-el7.repo \
 yum -y install \
 	unzip \
 	wget \
-	java-1.7.0-openjdk \
+	java-1.8.0-openjdk \
 	ice ice-python ice-servers
 
 yum -y install \
 	python-pip python-devel \
-	numpy scipy python-matplotlib python-pillow Cython \
-	gcc gcc-c++ \
-	hdf5-devel
-
-# Requires gcc-c++
-pip install numexpr
-# Requires gcc, Cython, hdf5-devel
-pip install tables
+	numpy scipy python-matplotlib python-pillow python-tables
 
 # Postgres, reconfigure to allow TCP connections
-yum -y install postgresql-server postgresql
+yum -y install http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-centos94-9.4-2.noarch.rpm
+yum -y install postgresql94-server postgresql94
 PGSETUP_INITDB_OPTIONS=--encoding=UTF8 postgresql-setup initdb
 sed -i.bak -re 's/^(host.*)ident/\1md5/' /var/lib/pgsql/data/pg_hba.conf
 
