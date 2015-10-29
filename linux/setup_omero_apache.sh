4087fe3de8ce76c26497384b660048f2432677be
Simon Li <spli@dundee.ac.uk>
2015-10-29 14:06:41 +0000

    web config apache-wsgi -> apache in 5.2

--- linux/setup_omero_apache.sh
+++ linux/setup_omero_apache.sh
@@ -5,5 +5,5 @@ set -e -u -x
 source settings.env
 
 OMERO.server/bin/omero config set omero.web.application_server wsgi
-OMERO.server/bin/omero web config apache-wsgi --http "$OMERO_WEB_PORT" > OMERO.server/apache.conf.tmp
+OMERO.server/bin/omero web config apache --http "$OMERO_WEB_PORT" > OMERO.server/apache.conf.tmp
 OMERO.server/bin/omero web syncmedia
