4f4fe645b1820df944f40b8206cb75b2c8e7a459
Simon Li <spli@dundee.ac.uk>
2015-10-29 14:56:24 +0000

    Add comment about wsgi-tcp being the default

--- linux/setup_omero.sh
+++ linux/setup_omero.sh
@@ -18,5 +18,6 @@ OMERO.server/bin/omero db script -f OMERO.server/db.sql --password "$OMERO_ROOT_
 
 psql -h localhost -U "$OMERO_DB_USER" "$OMERO_DB_NAME" < OMERO.server/db.sql
 
+# This is the default in 5.2 so could be left unset
 OMERO.server/bin/omero config set omero.web.application_server wsgi-tcp
 OMERO.server/bin/omero web config nginx-wsgi --http "$OMERO_WEB_PORT" > OMERO.server/nginx.conf.tmp
