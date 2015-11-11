58995422b1a6fb69717e2019eb83eb0dce3dbabc
jmoore <josh@glencoesoftware.com>
2015-11-11 20:35:17 +0100

    Enable caching

--- linux/test/docker-build.sh
+++ linux/test/docker-build.sh
@@ -7,14 +7,11 @@ fi
 
 set -e
 
-rm -rf omero-install-test
-mkdir omero-install-test
-cp ../*.sh ../*.env ../*init.d ../*cron omero-install-test
-zip -r $1/omero-install-test.zip omero-install-test
-rm -rf omero-install-test
+rm -rf $1/omero-install-test
+mkdir $1/omero-install-test
+cp -a ../*.sh ../*.env ../*init.d ../*cron $1/omero-install-test
 
 IMAGE=omero_install_test_${1%*/}
 echo "Building image $IMAGE"
-docker build -t $IMAGE --no-cache $1
+docker build -t $IMAGE $1
 echo "Test this image by running docker run -it [...] $IMAGE"
-
