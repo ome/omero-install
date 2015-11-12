e01ed630b406b9d6e80d73822caae80a0e88e173
Colin Blackburn <c.blackburn@dundee.ac.uk>
2015-11-12 16:27:58 +0000

    Add initial files
    duplicated from default nginx unbuntu1404 files with internal
    calls modified to effectively produce an nginx installation

---
+++ linux/install-ubuntu1404-apache.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e -u -x
+
+OMEROVER=omero
+#OMEROVER=omerodev
+
+source settings.env
+
+bash -eux dependencies-ubuntu1404.sh
+
+bash -eux system_setup.sh
+bash -eux setup_postgres.sh
+
+cp settings.env setup_$OMEROVER.sh ~omero
+cp setup_omero_apache.sh ~omero
+
+su - omero -c "bash -eux setup_$OMEROVER.sh"
+
+su - omero -c "bash -eux setup_omero_apache.sh"
+
+bash -eux setup_apache_ubuntu1404.sh
+
+#If you don't want to use the init.d scripts you can start OMERO manually:
+#su - omero -c "OMERO.server/bin/omero admin start"
+#su - omero -c "OMERO.server/bin/omero web start"
+
+bash -eux setup_omero_daemon_noweb_ubuntu1404.sh
+
+bash -eux setup_permissions.sh
+
+bash -eux setup_cron.sh
+
+#service omero start
+#service omero-web start
---
+++ linux/setup_apache_ubuntu1404.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get -y install nginx gunicorn
+
+# See setup_omero*.sh for the nginx config file creation
+
+cp ~omero/OMERO.server/nginx.conf.tmp /etc/nginx/sites-available/omero-web
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/omero-web /etc/nginx/sites-enabled/
+
+service nginx start
