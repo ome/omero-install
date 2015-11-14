1fe6aa4fcd0eb89c9028c80fc8294042e572acd9
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-11-14 21:16:34 +0000

    initial commit. Add java 1.8

---
+++ linux/dependencies-ubuntu1504.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y install \
+	unzip \
+	wget \
+	python-{imaging,matplotlib,numpy,pip,scipy,tables,virtualenv} \
+	openjdk-8-jre-headless \
+	ice-services python-zeroc-ice \
+	postgresql
+
+# Django
+pip install "Django<1.9"
+
+service postgresql start
