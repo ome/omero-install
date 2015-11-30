bb90ee5da1e510e207c3f7705a40955b943ca98f
Dominik Lindner <d.lindner@dundee.ac.uk>
2015-11-30 16:55:35 +0000

    Removed ICE_CONFIG,PYTHONPATH; added omero logout

--- osx/step04_test.sh
+++ osx/step04_test.sh
@@ -9,8 +9,6 @@ set -x
 export PATH=/usr/local/bin:$PATH
 export HTTPPORT=${HTTPPORT:-8080}
 export ROOT_PASSWORD=${ROOT_PASSWORD:-omero}
-export ICE_CONFIG=$(bin/brew --prefix omero52)/etc/ice.config
-export PYTHONPATH=$(bin/brew --prefix omero52)/lib/python
 
 # Start PostgreSQL
 pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log -w start
@@ -29,6 +27,7 @@ omero version | grep -v UNKNOWN
 omero login -s localhost -u root -w $ROOT_PASSWORD
 touch test.fake
 omero import test.fake
+omero logout
 
 # Test simple Web connection
 brew install wget
