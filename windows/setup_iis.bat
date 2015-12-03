3ad9c8e7362d1d8126bedbdb5202e619ca3b3b64
Dominik Lindner <d.lindner@dundee.ac.uk>
2015-12-03 14:54:33 +0000

    Added call to install requirements-py27-win.txt

--- windows/setup_iis.bat
+++ windows/setup_iis.bat
@@ -1,5 +1,8 @@
 start /w dism /Online /Enable-Feature /FeatureName:IIS-WebServerRole /FeatureName:IIS-WebServer /FeatureName:IIS-CommonHttpFeatures /FeatureName:IIS-Security /FeatureName:IIS-RequestFiltering /FeatureName:IIS-StaticContent /FeatureName:IIS-DefaultDocument /FeatureName:IIS-DirectoryBrowsing /FeatureName:IIS-HttpErrors /FeatureName:IIS-ApplicationDevelopment /FeatureName:IIS-ISAPIExtensions /FeatureName:IIS-ISAPIFilter /FeatureName:IIS-HealthAndDiagnostics /FeatureName:IIS-HttpLogging /FeatureName:IIS-Performance /FeatureName:IIS-HttpCompressionStatic /FeatureName:IIS-WebServerManagementTools /FeatureName:IIS-ManagementConsole /FeatureName:IIS-IIS6ManagementCompatibility /FeatureName:IIS-Metabase
 
+set PYTHONPIP=C:\Python27\Scripts\pip.exe
+%PYTHONPIP% install -r c:\OMERO.server\share\web\requirements-py27-win.txt
+
 start /w cscript j_unzip.vbs python-source\isapi_wsgi-0.4.2.zip
 pushd isapi_wsgi-0.4.2
 python setup.py install
