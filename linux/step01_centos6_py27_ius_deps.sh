404ba45cfdaa6721dbaa7cbee49585554ca63a7c
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-12-08 21:50:31 +0000

    Install virtualenv using python 2.6
    This is more likely to be the case.

--- linux/step01_centos6_py27_ius_deps.sh
+++ linux/step01_centos6_py27_ius_deps.sh
@@ -18,19 +18,30 @@ yum -y install \
 yum -y install \
 	python27 \
 	python27-devel \
-	python27-pip \
 	libjpeg-devel \
 	libpng-devel \
 	libtiff-devel \
 	hdf5-devel \
+	zlib-devel \
 	freetype-devel
 
+#some general requirements
+#yum -y install bzip2-devel openssl-devel
+
+# install pip and virtualenv using Python 2.6 
+yum -y install \
+	python-pip \
+	python-virtualenv
+
+
+#if virtualenv is not installed (unlikely)
+#yum -y install python27-pip
+#pip2.7 install virtualenv
 
 # TODO: this installs a lot of unecessary packages:
 yum -y groupinstall "Development Tools"
 
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
-pip2.7 install virtualenv
 
 
 # Postgres, reconfigure to allow TCP connections
