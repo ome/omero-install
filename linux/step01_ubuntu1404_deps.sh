18bbebcecd9221d26a466cfb0818797efd8f8949
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-12-09 12:34:58 +0000

    Add support for pillow 2.9.0
    Version installed by default 2.3.0

--- linux/step01_ubuntu1404_deps.sh
+++ linux/step01_ubuntu1404_deps.sh
@@ -4,10 +4,23 @@ apt-get update
 apt-get -y install \
 	unzip \
 	wget \
-	python-{imaging,matplotlib,numpy,pip,scipy,tables,virtualenv} \
+	python-{matplotlib,numpy,pip,scipy,tables,virtualenv} \
 	openjdk-7-jre-headless \
 	ice-services python-zeroc-ice \
 	postgresql
+
+# require to install Pillow
+apt-get -y install \
+	libtiff5-dev \
+	libjpeg8-dev \
+	zlib1g-dev \
+	libfreetype6-dev \
+	liblcms2-dev \
+	libwebp-dev \
+	tcl8.6-dev \
+	tk8.6-dev
+
+pip install --upgrade "Pillow<3.0"
 
 # Django
 pip install "Django<1.9"
