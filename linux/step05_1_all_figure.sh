5e549d85ab61bdcd5d7e38d7c42f19793a6d4b85
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-12-12 23:22:23 +0000

    Add comment

--- linux/step05_1_all_figure.sh
+++ linux/step05_1_all_figure.sh
@@ -1,14 +1,13 @@
 
 #!/bin/bash
-wget http://downloads.openmicroscopy.org/figure/1.2.0/figure-1.2.0.zip
 
-# Add OMERO web
+# Add OMERO.figure
 cd ~omero
 wget http://downloads.openmicroscopy.org/figure/1.2.0/figure-1.2.0.zip
 unzip -q figure-1.2.0.zip
 mv figure-1.2.0 OMERO.server/lib/python/omeroweb/figure
 
-# Install required package for web
+# Install required packages
 pip install reportlab markdown
 
 su - omero -c "OMERO.server/bin/omero config append omero.web.apps '\"figure\"'"
