15af8961d8a2136f43b2552403ea61d0b904bf77
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-12-12 22:25:36 +0000

    Add figure to the installation.

--- linux/docker_virtualenv_ius_apache22.sh
+++ linux/docker_virtualenv_ius_apache22.sh
@@ -44,6 +44,18 @@ cp settings.env ~omero
 cp setup_omero_apache22.sh ~omero
 su - omero -c "bash -eux setup_omero_apache22.sh"
 
+# Add OMERO web
+cd /home/omero
+wget http://downloads.openmicroscopy.org/figure/1.2.0/figure-1.2.0.zip
+unzip -q figure-1.2.0.zip
+mv figure-1.2.0 OMERO.server/lib/python/omeroweb/figure
+
+# Install required package for web
+/home/omero/omeroenv/bin/pip2.7 install reportlab markdown
+# add app
+su - omero -c "OMERO.server/bin/omero config append omero.web.apps '\"figure\"'"
+su - omero -c "OMERO.server/bin/omero config append omero.web.ui.top_links '[\"Figure\", \"figure_index\", {\"title\": \"Open Figure in new tab\", \"target\": \"figure\"}]'"
+
 # See setup_omero_apache.sh for the apache config file creation
 cp ~omero/OMERO.server/apache.conf.tmp /etc/httpd/conf.d/omero-web.conf
 
