d59c14cdde15ff9c201078d3e00b21c243a97a27
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-12-13 08:29:01 +0000

    Add option to specify parameter

--- linux/step05_1_all_figure.sh
+++ linux/step05_1_all_figure.sh
@@ -1,6 +1,14 @@
 
 #!/bin/bash
 
+# Read parameter
+VIRTUALENV=false
+for arg in "$@"; do
+	case "$arg" in ve)
+	VIRTUALENV=true;;
+	esac
+done
+
 # Add OMERO.figure
 cd ~omero
 wget http://downloads.openmicroscopy.org/figure/1.2.0/figure-1.2.0.zip
@@ -8,7 +16,11 @@ unzip -q figure-1.2.0.zip
 mv figure-1.2.0 OMERO.server/lib/python/omeroweb/figure
 
 # Install required packages
-pip install reportlab markdown
+if [ $VIRTUALENV = true ]; then
+	/home/omero/omeroenv/bin/pip2.7 install reportlab markdown
+else
+	pip install reportlab markdown
+fi
 
 su - omero -c "OMERO.server/bin/omero config append omero.web.apps '\"figure\"'"
 su - omero -c "OMERO.server/bin/omero config append omero.web.ui.top_links '[\"Figure\", \"figure_index\", {\"title\": \"Open Figure in new tab\", \"target\": \"figure\"}]'"
