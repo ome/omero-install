9e3b4d48bae82b19faeea5a0853db4af1c6dca9f
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-01-06 12:27:48 +0000

    Cap pillow version

--- linux/step01_centos6_py27_deps.sh
+++ linux/step01_centos6_py27_deps.sh
@@ -36,7 +36,10 @@ set -u
 easy_install pip
 
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
-pip install tables pillow matplotlib
+pip install tables matplotlib
+
+# Cap Pillow version due to a limitation in OMERO.figure with v3.0.0
+pip install "Pillow<3.0"
 
 # Django
 pip install "Django>=1.8,<1.9"
