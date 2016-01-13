e7501f6a93ad965c1c12cfb8a020bc9d238f003e
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-01-13 06:19:07 +0000

    Re-oder install step

--- linux/install_centos6_py27_apache24.sh
+++ linux/install_centos6_py27_apache24.sh
@@ -15,11 +15,11 @@ bash -eux step03_all_postgres.sh
 cp settings.env omero-centos6py27.env step04_centos6_py27_${OMEROVER}.sh ~omero
 su - omero -c "bash -eux step04_centos6_py27_${OMEROVER}.sh"
 
-bash -eux step05_centos6_py27_apache24.sh
-
 if [ $WEBAPPS = true ]; then
 	PY_ENV=scl bash -eux step05_1_all_webapps.sh
 fi
+
+bash -eux step05_centos6_py27_apache24.sh
 
 #If you don't want to use the init.d scripts you can start OMERO manually:
 #su - omero -c "OMERO.server/bin/omero admin start"
