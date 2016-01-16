1fd3f280608fa163f4f3c6c9c3997422de06703c
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-01-16 21:17:01 +0000

    Add support for new JAVAVER argument

--- linux/test/docker-build.sh
+++ linux/test/docker-build.sh
@@ -7,6 +7,7 @@ fi
 
 WEBAPPS=${WEBAPPS:-false}
 OMEROVER=${OMEROVER:-omero}
+JAVAVER=${JAVAVER:-open17}
 
 set -e
 
@@ -22,6 +23,7 @@ echo "Building image $IMAGE"
 if [[ $1 =~ "centos7" ]]; then
 	docker build -t $IMAGE --no-cache $1
 else
-	docker build -t $IMAGE --no-cache --build-arg OMEROVER=${OMEROVER} --build-arg WEBAPPS=${WEBAPPS} $1
+	docker build -t $IMAGE --no-cache --build-arg OMEROVER=${OMEROVER} \
+	--build-arg JAVAVER=${JAVAVER} --build-arg WEBAPPS=${WEBAPPS} $1
 fi
 echo "Test this image by running docker run -it [...] $IMAGE"
