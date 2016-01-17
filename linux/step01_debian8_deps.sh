f29d7e845bdcc98a058ac1c30adbada98b24ec80
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-01-17 19:02:24 +0000

    Add support for oracle Java 1.7/1.8

--- linux/step01_debian8_deps.sh
+++ linux/step01_debian8_deps.sh
@@ -3,9 +3,34 @@
 apt-get update
 apt-get -y install \
 	unzip \
-	wget \
-	python-{matplotlib,numpy,pip,scipy,tables,virtualenv} \
-	openjdk-7-jre-headless \
+	wget
+
+#install java
+if [ "$JAVAVER" = "open18" ]; then
+	echo 'deb http://httpredir.debian.org/debian jessie-backports main' > /etc/apt/sources.list.d/jessie-backports.list
+	apt-get -y install openjdk-8-jre-headless
+elif [ "$JAVAVER" = "oracle17" ]; then
+	echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" > /etc/apt/sources.list.d/webupd8team-java.list
+	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+	apt-get update
+	echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+	apt-get -y install oracle-java7-installer
+elif [ "$JAVAVER" = "oracle18" ]; then
+	echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" > /etc/apt/sources.list.d/webupd8team-java.list
+	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+	apt-get update
+	echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+	apt-get -y install oracle-java8-installer
+else
+	apt-get -y install openjdk-7-jre-headless
+fi
+
+apt-get -y install \
+	python-{matplotlib,numpy,pip,scipy,tables,virtualenv}
+
+apt-get -y install \
 	ice-services python-zeroc-ice \
 	postgresql
 
