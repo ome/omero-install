8ed5c1deaef263dd8ecc1f9e678eb09d07d8a18a
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-03-09 11:21:17 +0000

    Use markup instead of hard-code numbers.
    For number != 2.

--- linux/autogenerate.sh
+++ linux/autogenerate.sh
@@ -13,11 +13,15 @@ source settings.env
 EOF
 
 echo -en '\n' >> $file
-echo "#step 1: Dependencies" >> $file
+echo "#step 1: Install dependencies as root" >> $file
 if [ $OS = "centos7" ] ; then
-	line=$(sed -n '2,39p' step01_"$OS"_deps.sh)
+	number=$(sed -n '/#start-workaround/=' step01_"$OS"_deps.sh)
+	number=$((number-1))
+	line=$(sed -n '2,'$number'p' step01_"$OS"_deps.sh)
 	echo "$line" >> $file
-	line=$(sed -n '46,$p' step01_"$OS"_deps.sh)
+	number=$(sed -n '/#end-workaround/=' step01_"$OS"_deps.sh)
+	number=$((number+1))
+	line=$(sed -n ''$number',$p' step01_"$OS"_deps.sh)
 else
 	line=$(sed -n '2,$p' step01_"$OS"_deps.sh)
 fi
@@ -28,15 +32,22 @@ echo "#end step 1" >> $file
 if [ $OS = "centos6_py27_ius" ] ; then
 	echo -en '\n' >> $file
 	echo "#step 1.1: virtual env" >> $file
-	line=$(sed -n '4,23p' step03_"$OS"_virtualenv_deps.sh)
+	#find from where to start copying
+	start=$(sed -n '/#start-install/=' step03_"$OS"_virtualenv_deps.sh)
+	start=$((start+1))
+	number=$(sed -n '/#start-dev/=' step03_"$OS"_virtualenv_deps.sh)
+	number=$((number-1))
+	line=$(sed -n ''$start','$number'p' step03_"$OS"_virtualenv_deps.sh)
 	echo "$line" >> $file
-	line=$(sed -n '28p' step03_"$OS"_virtualenv_deps.sh)
+	number=$(sed -n '/#end-dev/=' step03_"$OS"_virtualenv_deps.sh)
+	number=$((number+1))
+	line=$(sed -n ''$number',$p' step03_"$OS"_virtualenv_deps.sh)
 	echo "$line" >> $file
 	echo "#end step 1.1:" >> $file
 fi
 
 echo -en '\n' >> $file
-echo "#step 2: Set-up" >> $file
+echo "#step 2: Set-up as root" >> $file
 if [ $OS = "centos6_py27" ] || [ $OS = "centos6_py27_ius" ] ; then
 	line=$(sed -n '2,$p' step02_"$OS"_setup.sh)
 else 
@@ -47,20 +58,27 @@ echo "#end step 2:" >> $file
 
 # postgres remove section
 echo -en '\n' >> $file
-echo "#step 3: Database user and database creation" >> $file
-line=$(sed -n '4,$p' step03_all_postgres.sh)
+echo "#step 3: Database user and database creation as root" >> $file
+#find from where to start copying
+start=$(sed -n '/#start-setup/=' step03_all_postgres.sh)
+start=$((start+1))
+line=$(sed -n ''$start',$p' step03_all_postgres.sh)
 echo "$line" >> $file
 echo "#end step 3" >> $file
 
 echo -en '\n' >> $file
-echo "#step 4: OMERO.server install" >> $file
+echo "#step 4: OMERO.server install as the omero system user" >> $file
 if [ $OS = "centos6_py27" ] || [ $OS = "centos6_py27_ius" ] ; then
 	var="${OS//_/}"
 	echo "cp settings.env omero-$var.env step04_$OS_omero.sh ~omero " >> $file
-	line=$(sed -n '6,$p' step04_"$OS"_omero.sh)
+	start=$(sed -n '/#start-install/=' step04_"$OS"_omero.sh)
+	start=$((start+1))
+	line=$(sed -n ''$start',$p' step04_"$OS"_omero.sh)
 else 
 	echo "cp settings.env step04_all_omero.sh ~omero " >> $file
-	line=$(sed -n '6,$p' step04_all_omero.sh)
+	start=$(sed -n '/#start-install/=' step04_all_omero.sh)
+	start=$((start+1))
+	line=$(sed -n ''$start',$p' step04_all_omero.sh)
 fi
 echo "$line" >> $file
 echo "#end step 4" >> $file
@@ -71,12 +89,10 @@ if [ $OS = "debian8" ] ; then
 fi
 
 echo -en '\n' >> $file
-echo "#step 5: Web server nginx" >> $file
-if [ $OS = "centos6_py27_ius" ] ; then
-	line=$(sed -n '4,$p' step05_"$v"_nginx.sh)
-else
-	line=$(sed -n '2,$p' step05_"$v"_nginx.sh)
-fi 
+echo "#step 5: Install Web server, Nginx, as root" >> $file
+start=$(sed -n '/#start-install/=' step05_"$v"_nginx.sh)
+start=$((start+1))
+line=$(sed -n ''$start',$p' step05_"$v"_nginx.sh)
 
 echo "$line" >> $file
 echo "#end step 5" >> $file
@@ -86,14 +102,16 @@ if [ $OS = "centos6_py27" ] || [ $OS = "centos6_py27_ius" ] ; then
 fi
 
 echo -en '\n' >> $file
-echo "#step 6: scripts to start OMERO and OMERO.web automatically" >> $file
+echo "#step 6: Scripts to start OMERO and OMERO.web automatically as root" >> $file
 line=$(sed -n '2,$p' step06_"$v"_daemon.sh)
 echo "$line" >> $file
 echo "#end step 6" >> $file
 
 echo -en '\n' >> $file
-echo "#step 7: Securing OMERO" >> $file
-line=$(sed -n '6,$p' step07_all_perms.sh)
+echo "#step 7: Securing OMERO as root" >> $file
+start=$(sed -n '/#start/=' step07_all_perms.sh)
+start=$((start+1))
+line=$(sed -n ''$start',$p' step07_all_perms.sh)
 echo "$line" >> $file
 echo "#end step 7" >> $file
 
