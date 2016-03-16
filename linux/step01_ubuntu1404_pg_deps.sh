0a73ceb24b803eb486030fcd4bad45f8ac7ec4a3
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-03-16 14:31:56 +0000

    Add support for pg 9.5 ubuntu

--- linux/step01_ubuntu1404_pg_deps.sh
+++ linux/step01_ubuntu1404_pg_deps.sh
@@ -7,9 +7,16 @@ if [ "$PGVER" = "pg94" ]; then
 	#start-recommended
 	apt-get -y install apt-transport-https
 	add-apt-repository -y "deb https://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
-	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 	apt-get update
 	apt-get -y install postgresql-9.4
 	service postgresql start
 	#end-recommended
+elif [ "$PGVER" = "pg95" ]; then
+	apt-get -y install apt-transport-https
+	add-apt-repository -y "deb https://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main 9.5"
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+	apt-get update
+	apt-get -y install postgresql-9.5
+	service postgresql start
 fi
