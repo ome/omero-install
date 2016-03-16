cfaf0d1289d3913d2472a34c824e09897d91ec60
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-03-16 13:52:54 +0000

    Add support for postgres 9.5

--- linux/step01_centos6_pg_deps.sh
+++ linux/step01_centos6_pg_deps.sh
@@ -14,4 +14,13 @@ if [ "$PGVER" = "pg94" ]; then
 	chkconfig postgresql-9.4 on
 	service postgresql-9.4 start
 	#end-recommended
+elif [ "$PGVER" = "pg95" ]; then
+	# Postgres, reconfigure to allow TCP connections
+	yum -y install http://yum.postgresql.org/9.5/redhat/rhel-6-x86_64/pgdg-centos95-9.5-2.noarch.rpm
+	yum -y install postgresql95-server postgresql95
+
+	service postgresql-9.5 initdb
+	sed -i.bak -re 's/^(host.*)ident/\1md5/' /var/lib/pgsql/9.5/data/pg_hba.conf
+	chkconfig postgresql-9.5 on
+	service postgresql-9.5 start
 fi
