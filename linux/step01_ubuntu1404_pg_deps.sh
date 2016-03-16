ed074ab8dfb17a1d300689fdcf077e65c67f55de
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-03-16 11:57:11 +0000

    Install the recommended postgres version.
    The default version installed using:
    apt-get -y install postgresql is 9.3.
    Version marked as deprecated in our requirements page.

--- linux/step01_ubuntu1404_pg_deps.sh
+++ linux/step01_ubuntu1404_pg_deps.sh
@@ -5,6 +5,10 @@ PGVER=${PGVER:-pg94}
 # Postgres installation
 if [ "$PGVER" = "pg94" ]; then
 	#start-recommended
+	apt-get -y install apt-transport-https
+	add-apt-repository -y "deb https://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+	apt-get update
 	apt-get -y install postgresql-9.4
 	service postgresql start
 	#end-recommended
