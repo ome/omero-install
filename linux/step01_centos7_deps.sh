1051c3ade59aa10acae1c9a7fb51f1fbe49e16c0
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-03-18 13:26:39 +0000

    Do no upgrade to the latest. This creates a locale error
    To be investigated.

--- linux/step01_centos7_deps.sh
+++ linux/step01_centos7_deps.sh
@@ -4,9 +4,6 @@ yum -y install \
 	python-pip python-devel python-virtualenv \
 	numpy scipy python-matplotlib python-tables
 
-# upgrade pip to run the latest
-pip install --upgrade pip
-
 yum -y install \
 	zlib-devel \
 	libjpeg-devel \
