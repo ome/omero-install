82ca7ff7dddf5584adc860c41a5a9c772a2af975
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-03-21 15:59:25 +0000

    Combine files.

--- linux/step04_all_omero.sh
+++ linux/step04_all_omero.sh
@@ -2,16 +2,26 @@
 
 set -e -u -x
 
+OMEROVER=${OMEROVER:-omero}
+
 source settings.env
 
 #start-install
-
-SERVER=http://downloads.openmicroscopy.org/latest/omero5/server-ice35.zip
-
-wget $SERVER
-unzip -q server-ice35.zip
+virtualenv omego
+omego/bin/pip install omego
+if [ $OMEROVER = omerodev ]; then
+	omego/bin/omego download --branch OMERO-DEV-latest server
+elif [ $OMEROVER = omeromerge ]; then
+	BRANCH=OMERO-DEV-merge-build
+	omego/bin/omego download --branch OMERO-DEV-merge-build server
+else
+	#start-release
+	omego/bin/omego download --release latest server
+	#end-release
+fi
 ln -s OMERO.server-*/ OMERO.server
 
+# configure
 OMERO.server/bin/omero config set omero.data.dir "$OMERO_DATA_DIR"
 OMERO.server/bin/omero config set omero.db.name "$OMERO_DB_NAME"
 OMERO.server/bin/omero config set omero.db.user "$OMERO_DB_USER"
