023214e9dd62dcbea02ceecf3425d1ba3e304cac
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-03-28 10:04:05 +0100

    Install ice36 from source on centos7

--- linux/step01_centos7_ice_deps.sh
+++ linux/step01_centos7_ice_deps.sh
@@ -15,4 +15,32 @@ elif [ "$ICEVER" = "ice35-devel" ]; then
 	http://download.zeroc.com/Ice/3.5/el7/zeroc-ice-el7.repo
 
 	yum -y install ice ice-python-devel ice-java-devel ice-servers
+elif [ "$ICEVER" = "ice36" ]; then
+	curl -o /etc/yum.repos.d/zeroc-ice-el7.repo \
+	http://download.zeroc.com/Ice/3.6/el7/zeroc-ice-el7.repo
+	yum -y groupinstall "Development tools"
+	yum -y install openssl-devel bzip2-devel expat-devel
+	yum -y install mcpp-devel lmdb-devel libdb-cxx-devel
+	
+	mkdir /tmp/ice-download
+	cd /tmp/ice-download
+	URL=https://github.com/zeroc-ice/ice/archive/3.6.zip
+	NAME_ZIP=${URL##*/}
+	wget $URL
+	unzip -q $NAME_ZIP
+	rm $NAME_ZIP
+	cd ice-3.6
+	cd cpp
+	make && make install
+	cd ../python
+	make && make install
+	#zeroc-ice
+	cd ../..
+	wget https://pypi.python.org/packages/source/z/zeroc-ice/zeroc-ice-3.6.1.zip#md5=a8c5a7782826c7b342e13c870ac59c7b
+	unzip -q zeroc-ice-3.6.1.zip
+	cd zeroc-ice-3.6.1
+	python setup.py install
+
+	echo /opt/Ice-3.6.2/lib64 > /etc/ld.so.conf.d/ice-x86_64.conf
+	ldconfig
 fi
