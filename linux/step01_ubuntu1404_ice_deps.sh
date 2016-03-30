14ea32825d90d4c8fa8387a2ad7dd3a6b8f09f35
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-03-30 12:41:02 +0100

    Build ice from source.

--- linux/step01_ubuntu1404_ice_deps.sh
+++ linux/step01_ubuntu1404_ice_deps.sh
@@ -14,23 +14,28 @@ elif [ "$ICEVER" = "ice36" ]; then
 	apt-get -y install libssl-dev libbz2-dev libmcpp-dev libdb++-dev libdb-dev libdb-java
 	mkdir /tmp/ice-download
 	cd /tmp/ice-download
-	#to be modified and rename
-	wget http://users.openmicroscopy.org.uk/~jburel/Ice-3.6.1-b1-ubuntu1404-amd64.tar.gz
 
-	tar -zxvf /tmp/ice-download/Ice-3.6.1-b1-ubuntu1404-amd64.tar.gz
-
-	# so we don't have to update ICE_HOME
-	mv opt/Ice-3.6.1 /opt/Ice-3.6.2
-
-	# make path to Ice globally accessible
-	# if globally set, there is no need to export LD_LIBRARY_PATH
-	echo /opt/Ice-3.6.2/lib64 > /etc/ld.so.conf.d/ice-x86_64.conf
-	echo /opt/Ice-3.6.2/lib > /etc/ld.so.conf.d/ice-x86_64.conf
-	ldconfig
-
+	URL=https://github.com/zeroc-ice/ice/archive/3.6.zip
+	NAME_ZIP=${URL##*/}
+	wget $URL
+	unzip -q $NAME_ZIP
+	rm $NAME_ZIP
+	cd ice-3.6
+	cd cpp
+	make && make install
+	cd ../python
+	make && make install
+	#zeroc-ice
+	cd ../..
+	
 	# build zeroc-ice
 	wget https://pypi.python.org/packages/source/z/zeroc-ice/zeroc-ice-3.6.1.zip#md5=a8c5a7782826c7b342e13c870ac59c7b
 	unzip -q zeroc-ice-3.6.1.zip
 	cd zeroc-ice-3.6.1
 	python setup.py install
+	# make path to Ice globally accessible
+	# if globally set, there is no need to export LD_LIBRARY_PATH
+	echo /opt/Ice-3.6.2/lib64 > /etc/ld.so.conf.d/ice-x86_64.conf
+	echo /opt/Ice-3.6.2/lib > /etc/ld.so.conf.d/ice-x86_64.conf
+	ldconfig
 fi
