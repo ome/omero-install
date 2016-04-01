f8dec2499d0e7eca73ea81aeba0571ee3ae523be
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-04-01 14:27:09 +0100

    Add debian8 specific file to install nginx.

--- linux/install_debian8_nginx.sh
+++ linux/install_debian8_nginx.sh
@@ -33,7 +33,7 @@ su - omero -c "OMEROVER=$OMEROVER bash -eux step04_all_omero.sh"
 
 su - omero -c "bash setup_omero_db.sh"
 
-bash -eux step05_ubuntu1404_nginx.sh
+bash -eux step05_debian8_nginx.sh
 
 if [ $WEBAPPS = true ]; then
 	bash -eux step05_1_all_webapps.sh
