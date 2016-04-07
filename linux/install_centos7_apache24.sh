6a41bafb3dafae8bc9a28a249cef757cf2f855cd
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-04-07 19:19:24 +0100

    Handle the various versions of postgres

--- linux/install_centos7_apache24.sh
+++ linux/install_centos7_apache24.sh
@@ -44,7 +44,7 @@ bash -eux step05_centos7_apache24.sh
 
 bash -eux setup_centos_selinux.sh
 
-bash -eux step06_centos7_daemon.sh
+PGVER=$PGVER bash -eux step06_centos7_daemon.sh
 
 #systemctl start omero.service
 #systemctl start omero-web.service
