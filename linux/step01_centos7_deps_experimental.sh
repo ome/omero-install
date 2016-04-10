cffd8129f27043a689251ac2a1d43e354910d5a3
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-04-10 23:34:14 +0100

    enable redis

--- linux/step01_centos7_deps_experimental.sh
+++ linux/step01_centos7_deps_experimental.sh
@@ -3,3 +3,8 @@
 yum -y install redis python-redis
 
 pip install django-redis-cache>=1.6.5
+
+systemctl enable redis.service
+if [ ! "${container:-}" = docker ]; then
+    systemctl start redis.service
+fi
