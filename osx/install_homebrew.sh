b5dccf9cb358b7bae4b8cc319a16e7f474933f66
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-04-12 23:58:13 +0100

    add redis for osx in a separate PR

--- osx/install_homebrew.sh
+++ osx/install_homebrew.sh
@@ -2,14 +2,7 @@
 
 set -e -u -x
 
-EXPERIMENTAL=${EXPERIMENTAL:-false}
-
 bash -eux step01_deps.sh
 bash -eux step02_omero.sh
-
-if $EXPERIMENTAL ; then
-    bash -eux step01_deps_experimental.sh
-fi
-
 bash -eux step03_nginx.sh
 bash -eux step04_test.sh
