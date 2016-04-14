9c276275c5e3f5039c2cbc04b14e1dc48cdc42c5
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-04-14 14:25:43 +0100

    Improve installation on ubuntu/debian
    the zeroc link is now working.

--- linux/step01_ubuntu1404_ice_deps.sh
+++ linux/step01_ubuntu1404_ice_deps.sh
@@ -12,26 +12,13 @@ elif [ "$ICEVER" = "ice36" ]; then
 	# install git to build omero
 	apt-get -y install git
 
-	apt-get -y install libssl-dev libbz2-dev libmcpp-dev libdb++-dev libdb-dev libdb-java
-	mkdir /tmp/ice-download
-	cd /tmp/ice-download
+	apt-get -y install db5.3-util
+	apt-get -y install libssl-dev libbz2-dev libmcpp-dev libdb++-dev libdb-dev
 
-	URL=https://github.com/zeroc-ice/ice/archive/v3.6.2.zip
-	NAME_ZIP=${URL##*/}
-	wget $URL
-	unzip -q $NAME_ZIP
-	rm $NAME_ZIP
-	cd ice-3.6.2
-	cd cpp
-	make && make install
-	#zeroc-ice
-	cd ../..
-	
+	apt-key adv --keyserver keyserver.ubuntu.com --recv 5E6DA83306132997
+	apt-add-repository "deb http://zeroc.com/download/apt/ubuntu`lsb_release -rs` stable main"
+	apt-get update
+	apt-get -y install zeroc-ice-all-runtime zeroc-ice-all-dev
 	# build zeroc-ice
 	pip install zeroc-ice
-
-	# make path to Ice globally accessible
-	# if globally set, there is no need to export LD_LIBRARY_PATH
-	echo /opt/Ice-3.6.2/lib64 > /etc/ld.so.conf.d/ice-x86_64.conf
-	ldconfig
 fi
