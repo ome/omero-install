f3e2d087bc1f8f70394e4c27264f843a9bd277aa
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-04-18 10:16:14 +0100

    Remove cento6s from the list of generated walkthrough
    It is still possible to generate the walkthrough for centos6
    but it won't be generated when the ALL flag is used.

--- linux/autogenerate.sh
+++ linux/autogenerate.sh
@@ -12,7 +12,7 @@ echo "${l}"
 
 #generate the walkthrough for all supported os
 function generate_all() {
-	values=(centos7 centos6 centos6_py27 centos6_py27_ius ubuntu1404 debian8)
+	values=(centos7 centos6_py27 centos6_py27_ius ubuntu1404 debian8)
 	for os in "${values[@]}"; do
   		echo "${os}"
   		 generate ${os}
