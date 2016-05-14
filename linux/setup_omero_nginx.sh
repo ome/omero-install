0b7ebb1bec6c713442ff2795d1fe3c68e9693f0b
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-05-14 06:36:25 +0100

    Remove code introduced during testing.

--- linux/setup_omero_nginx.sh
+++ linux/setup_omero_nginx.sh
@@ -5,7 +5,5 @@ set -e -u -x
 source `dirname $0`/settings.env
 
 #start-config
-#OMERO.server/bin/omero config set omero.web.application_server wsgi-tcp
-#OMERO.server/bin/omero web config $NGINXCMD --http "$OMERO_WEB_PORT" > OMERO.server/nginx.conf.tmp
-OMERO.server/bin/omero config set omero.web.application_server fastcgi-tcp
-OMERO.server/bin/omero web config $NGINXCMD --system --http "$OMERO_WEB_PORT" > OMERO.server/nginx.conf.tmp
+OMERO.server/bin/omero config set omero.web.application_server wsgi-tcp
+OMERO.server/bin/omero web config $NGINXCMD --http "$OMERO_WEB_PORT" > OMERO.server/nginx.conf.tmp
