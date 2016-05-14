7f495818870cfd33c29f2c3e518679c4176ec016
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-05-14 20:32:22 +0100

    Specify parameter for 5.1

--- linux/step05_centos6_apache22.sh
+++ linux/step05_centos6_apache22.sh
@@ -8,11 +8,19 @@ cp setup_omero_apache22.sh ~omero
 yum -y install httpd mod_wsgi
 
 # Install OMERO.web requirements
-pip install -r ~omero/OMERO.server/share/web/requirements-py26-apache.txt
+file=~omero/OMERO.server/share/web/requirements-py26-apache.txt
+p=apache
+# introduce in 5.2.0
+if [ -f $file ]; then
+	pip install -r $file
+else
+	#for version 5.1.x
+	p=apache-wsgi
+fi
 
 #start-setup-as-omero
 # See setup_omero_apache.sh for the apache config file creation
-su - omero -c "bash -eux setup_omero_apache22.sh"
+su - omero -c "bash -eux setup_omero_apache22.sh $p"
 #end-setup-as-omero
 
 cp ~omero/OMERO.server/apache.conf.tmp /etc/httpd/conf.d/omero-web.conf
