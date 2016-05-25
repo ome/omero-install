84c94f952a8c7541332e62cfd235806f666dfbd3
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-05-25 12:48:06 +0100

    Remove ref to windows

--- windows/setup_iis.bat
+++
@@ -1,7 +0,0 @@
-start /w dism /Online /Enable-Feature /FeatureName:IIS-WebServerRole /FeatureName:IIS-WebServer /FeatureName:IIS-CommonHttpFeatures /FeatureName:IIS-Security /FeatureName:IIS-RequestFiltering /FeatureName:IIS-StaticContent /FeatureName:IIS-DefaultDocument /FeatureName:IIS-DirectoryBrowsing /FeatureName:IIS-HttpErrors /FeatureName:IIS-ApplicationDevelopment /FeatureName:IIS-ISAPIExtensions /FeatureName:IIS-ISAPIFilter /FeatureName:IIS-HealthAndDiagnostics /FeatureName:IIS-HttpLogging /FeatureName:IIS-Performance /FeatureName:IIS-HttpCompressionStatic /FeatureName:IIS-WebServerManagementTools /FeatureName:IIS-ManagementConsole /FeatureName:IIS-IIS6ManagementCompatibility /FeatureName:IIS-Metabase
-
-start /w cscript j_unzip.vbs python-source\isapi_wsgi-0.4.2.zip
-pushd isapi_wsgi-0.4.2
-python setup.py install
-popd
-
