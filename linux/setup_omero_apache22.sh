c3370fb15c21034fb4e1a768f9ab13be4f822a1a
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-09-08 11:15:00 +0100

    avoid stderr when generating webserver config

--- linux/setup_omero_apache22.sh
+++ linux/setup_omero_apache22.sh
@@ -6,5 +6,5 @@ source settings.env
 
 #start-config
 OMERO.server/bin/omero config set omero.web.application_server wsgi
-OMERO.server/bin/omero web config apache --http "$OMERO_WEB_PORT" > OMERO.server/apache.conf.tmp
+OMERO.server/bin/omero web config apache --http "$OMERO_WEB_PORT" 2>&1 > OMERO.server/apache.conf.tmp
 OMERO.server/bin/omero web syncmedia
--- linux/setup_omero_apache24.sh
+++ linux/setup_omero_apache24.sh
@@ -6,5 +6,5 @@ source `dirname $0`/settings.env
 
 #start-config
 OMERO.server/bin/omero config set omero.web.application_server wsgi
-OMERO.server/bin/omero web config apache24 --http "$OMERO_WEB_PORT" > OMERO.server/apache.conf.tmp
+OMERO.server/bin/omero web config apache24 --http "$OMERO_WEB_PORT" 2>&1 > OMERO.server/apache.conf.tmp
 OMERO.server/bin/omero web syncmedia
