3d960680c152a76e587d6fa953ba8ff693b23a55
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-12-08 11:04:43 +0000

    Remove duplicated line

--- linux/step04_all_omero.sh
+++ linux/step04_all_omero.sh
@@ -69,7 +69,6 @@ OMERO.server/bin/omero config set omero.data.dir "$OMERO_DATA_DIR"
 OMERO.server/bin/omero config set omero.db.name "$OMERO_DB_NAME"
 OMERO.server/bin/omero config set omero.db.user "$OMERO_DB_USER"
 OMERO.server/bin/omero config set omero.db.pass "$OMERO_DB_PASS"
-OMERO.server/bin/omero db script -f OMERO.server/db.sql "" "" "$OMERO_ROOT_PASS"
 #start-db
 
 if $(is_less_than $OMEROVER 5.1); then
