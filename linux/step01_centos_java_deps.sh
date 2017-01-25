bbbb7968042dcb0398246af6cd3a7a93dd8d904b
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-01-25 16:36:06 +0000

    Install java9 on centos

--- linux/step01_centos_java_deps.sh
+++ linux/step01_centos_java_deps.sh
@@ -26,4 +26,18 @@ elif [ "$JAVAVER" = "openjdk18-devel" ]; then
 	yum -y install java-1.8.0-openjdk-devel
 elif [ "$JAVAVER" = "openjdk17-devel" ]; then
 	yum -y install java-1.7.0-openjdk-devel
+elif [ "$JAVAVER" = "oracle19" ]; then
+	cd /opt
+	wget --no-cookies \
+	--no-check-certificate \
+	--header "Cookie: oraclelicense=accept-securebackup-cookie" \
+	"http://www.java.net/download/java/jdk9/archive/116/binaries/jdk-9-ea+116_linux-x64_bin.tar.gz" \
+	-O jdk-9-linux-x64.tar.gz
+	tar -zxvf jdk-9-linux-x64.tar.gz
+	cd jdk-9
+	alternatives --install /usr/bin/java java /opt/jdk-9/bin/java 2
+	alternatives --install /usr/bin/jar jar /opt/jdk-9/bin/jar 2
+	alternatives --install /usr/bin/javac javac /opt/jdk-9/bin/javac 2
+	alternatives --set jar /opt/jdk-9/bin/jar
+	alternatives --set javac /opt/jdk-9/bin/javac
 fi
