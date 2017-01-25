1381e5d735983f752c0acbf3e3c45dcde0128b3f
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-01-25 16:36:25 +0000

    Install java9 on ubuntu

--- linux/step01_ubuntu1404_java_deps.sh
+++ linux/step01_ubuntu1404_java_deps.sh
@@ -32,4 +32,11 @@ elif [ "$JAVAVER" = "openjdk18-devel" ]; then
 	add-apt-repository -y ppa:openjdk-r/ppa
 	apt-get update
 	apt-get -y install openjdk-8-jdk
+elif [ "$JAVAVER" = "oracle19" ]; then
+	apt-get -y install software-properties-common
+	add-apt-repository -y ppa:webupd8team/java
+	apt-get update
+	echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+	echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+	apt-get -y install oracle-java9-installer
 fi
