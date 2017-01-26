a135c45f75103d97be2457413ecacccc24d77426
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-01-26 09:23:09 +0000

    Add support for pg 9.6

--- linux/step01_ubuntu1604_pg_deps.sh
+++ linux/step01_ubuntu1604_pg_deps.sh
@@ -17,4 +17,11 @@ elif [ "$PGVER" = "pg95" ]; then
 	apt-get update
 	apt-get -y install postgresql-9.5
 	service postgresql start
+elif [ "$PGVER" = "pg96" ]; then
+	apt-get -y install apt-transport-https
+	add-apt-repository -y "deb https://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main 9.6"
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+	apt-get update
+	apt-get -y install postgresql-9.6
+	service postgresql start
 fi
