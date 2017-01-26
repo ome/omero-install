7ef914c274918df267f1c353ec530e1909474a51
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-01-26 12:53:06 +0000

    Add service to start pg 96

--- linux/step06_centos7_daemon.sh
+++ linux/step06_centos7_daemon.sh
@@ -4,6 +4,8 @@ PGVER=${PGVER:-pg94}
 
 if [ "$PGVER" = "pg95" ]; then
 	cp omero-pg95-systemd.service /etc/systemd/system/omero.service
+elif [ "$PGVER" = "pg96" ]; then
+	cp omero-pg96-systemd.service /etc/systemd/system/omero.service
 elif [ "$PGVER" = "pg94" ]; then
 	#start-recommended
 	cp omero-systemd.service /etc/systemd/system/omero.service
