05706b9b35e1e5e52623e2e8aff5b1304201aa3b
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-02-21 13:20:41 +0000

    Fix pg version 16.04

--- linux/step01_ubuntu1604_pg_deps.sh
+++ linux/step01_ubuntu1604_pg_deps.sh
@@ -4,16 +4,16 @@ PGVER=${PGVER:-pg94}
 
 
 # Postgres installation
-if [ "$PGVER" = "pg94" ]; then
+if [ "$PGVER" = "pg95" ]; then
 	apt-get update
 	apt-get -y install postgresql
 	service postgresql start
-elif [ "$PGVER" = "pg95" ]; then
+elif [ "$PGVER" = "pg94" ]; then
 	apt-get -y install apt-transport-https
-	add-apt-repository -y "deb https://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main 9.5"
+	add-apt-repository -y "deb https://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main 9.4"
 	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 	apt-get update
-	apt-get -y install postgresql-9.5
+	apt-get -y install postgresql-9.4
 	service postgresql start
 elif [ "$PGVER" = "pg96" ]; then
 	#start-recommended
