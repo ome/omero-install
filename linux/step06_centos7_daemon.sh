e90d9d7cf55e877af9ef8bfb4dcfdf19e28df8ad
Simon Li <spli@dundee.ac.uk>
2017-03-21 10:33:18 +0000

    Update step06_centos7_daemon.sh to use single omero-systemd.service file

--- linux/step06_centos7_daemon.sh
+++ linux/step06_centos7_daemon.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-PGVER=${PGVER:-pg94}
-
-if [ "$PGVER" = "pg95" ]; then
-	cp omero-pg95-systemd.service /etc/systemd/system/omero.service
-elif [ "$PGVER" = "pg96" ]; then
-	cp omero-pg96-systemd.service /etc/systemd/system/omero.service
-elif [ "$PGVER" = "pg94" ]; then
-	#start-recommended
-	cp omero-systemd.service /etc/systemd/system/omero.service
-fi
+#start-recommended
+cp omero-systemd.service /etc/systemd/system/omero.service
 cp omero-web-systemd.service /etc/systemd/system/omero-web.service
 if [ ! "${container:-}" = docker ]; then
     systemctl daemon-reload
