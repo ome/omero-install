3f73363f3f39e6f85dff9bc2470a7163938ea05a
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-04-04 21:06:03 +0100

    Stop the correct version of pg

--- linux/docker_shutdown_centos6.sh
+++ linux/docker_shutdown_centos6.sh
@@ -4,6 +4,8 @@
 PGVER=${PGVER:-pg94}
 if [ "$PGVER" = "pg94" ]; then
 	service postgresql-9.4 stop
-elif [ "$PGVER" = "pg94" ]; then
+elif [ "$PGVER" = "pg95" ]; then
 	service postgresql-9.5 stop
+elif [ "$PGVER" = "pg96" ]; then
+	service postgresql-9.6 stop
 fi
