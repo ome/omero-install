f66ec2aceb82ebfadd030de5af9814c9f4f3b44e
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-06-15 22:22:12 +0100

    Update file

--- linux/step01_debian9_pg_deps.sh
+++ linux/step01_debian9_pg_deps.sh
@@ -3,9 +3,8 @@
 PGVER=${PGVER:-pg96}
 
 # Postgres installation
-if [ "$PGVER" = "pg96" ]; then
-	#start-recommended
-	apt-get -y install postgresql
-	service postgresql start
-	#end-recommended
-fi
+# 9.6
+#start-recommended
+apt-get -y install postgresql
+service postgresql start
+#end-recommended
