d110b72d77cd6eee7cf238b5c014333a04cd1015
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-06-28 14:59:21 +0100

    Use common file

--- linux/install_debian9_nginx.sh
+++ linux/install_debian9_nginx.sh
@@ -14,7 +14,7 @@ bash -eux step01_ubuntu_init.sh
 # install java
 bash -eux step01_debian9_java_deps.sh
 
-bash -eux step01_debian8_deps.sh
+bash -eux step01_debian_deps.sh
 
 # install ice
 bash -eux step01_debian9_ice_deps.sh
