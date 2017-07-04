f3b011f7e74175931afd8003934de60041e6525a
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-07-04 16:11:33 +0200

    Remove .bak file

--- linux/step04_omero_patch_openssl.sh
+++ linux/step04_omero_patch_openssl.sh
@@ -4,4 +4,5 @@
 sed -i.bak 's/\("IceSSL.Ciphers".*ADH\)/\1:@SECLEVEL=0/' OMERO.server/lib/python/omero/clients.py OMERO.server/etc/templates/grid/templates.xml
 #end-seclevel
 #clean files
-rm *.bak
+rm OMERO.server/lib/python/omero/clients.py.bak
+rm OMERO.server/etc/templates/grid/templates.xml.bak
