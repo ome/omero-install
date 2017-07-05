b28d4ac4dbc9d5b18323e098afc0af5f02164e40
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-07-05 10:22:49 +0200

    Do not include pg start in the walkthrough

--- linux/step01_debian9_pg_deps.sh
+++ linux/step01_debian9_pg_deps.sh
@@ -5,5 +5,5 @@ PGVER=${PGVER:-pg96}
 # Postgres installation: version installed will be 9.6
 #start-recommended
 apt-get -y install postgresql
-service postgresql start
 #end-recommended
+service postgresql start
