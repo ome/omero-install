104d2c0dad85968ee2d031bf0b5fc63768ae12f3
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-08-29 06:44:34 +0100

    Install libjpeg

--- linux/step01_debian_deps.sh
+++ linux/step01_debian_deps.sh
@@ -6,6 +6,6 @@ apt-get -y install \
 pip install --upgrade pip
 
 #start-web-dependencies
-apt-get -y install zlib1g-dev
+apt-get -y install zlib1g-dev libjpeg-dev
 apt-get -y install python-{pillow,numpy}
 #end-web-dependencies
