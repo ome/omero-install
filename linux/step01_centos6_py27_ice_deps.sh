a1e8a852e202d5147fb8cad5c15d8c4fa4111620
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-09-18 09:30:59 +0100

    Remove wget command

--- linux/step01_centos6_py27_ice_deps.sh
+++ linux/step01_centos6_py27_ice_deps.sh
@@ -27,9 +27,7 @@ if [[ "$ICEVER" =~ "ice35" ]]; then
     #end-supported
 elif [ "$ICEVER" = "ice36" ]; then
 	#start-recommended
-	cd /etc/yum.repos.d
-	wget https://zeroc.com/download/rpm/zeroc-ice-el6.repo
-	curl -sL https://zeroc.com/download/Ice/3.6/el6/zeroc-ice3.6.repo > \
+    curl -sL https://zeroc.com/download/Ice/3.6/el6/zeroc-ice3.6.repo > \
     /etc/yum.repos.d/zeroc-ice3.6.repo
 
 	yum -y install gcc-c++
