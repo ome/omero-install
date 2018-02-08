563b9cc6ff6a654f3cc96c010cfb1311a874bfd2
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-02-08 13:00:45 +0000

    Install dependencies

--- linux/step01_ubuntu_ice_deps.sh
+++ linux/step01_ubuntu_ice_deps.sh
@@ -9,7 +9,7 @@ if [[ "$ICEVER" =~ "ice35" ]]; then
 	apt-get -y install ice-services python-zeroc-ice
 	#end-supported
 elif [ "$ICEVER" = "ice36" ]; then
-	apt-get -y install python-dev
+	apt-get -y install python-dev build-essential
 	#start-recommended
 	apt-get -y install db5.3-util
 	apt-get -y install libssl-dev libbz2-dev libmcpp-dev libdb++-dev libdb-dev
