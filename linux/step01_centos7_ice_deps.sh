e0e3f9f1d46beab8b14194170d02f8591f573d71
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-02-10 10:46:07 +0000

    Remove tabs

--- linux/step01_centos7_ice_deps.sh
+++ linux/step01_centos7_ice_deps.sh
@@ -26,8 +26,8 @@ elif [ "$ICEVER" = "ice36" ]; then
     yum -y install ice-all-runtime
 
     pip install "zeroc-ice>3.5,<3.7"
-	#end-recommended
-	# reset the locale
+    #end-recommended
+    # reset the locale
     localedef -i en_US -f UTF-8 en_US.UTF-8
 elif [ "$ICEVER" = "ice36-devel" ]; then
     curl -sL https://zeroc.com/download/Ice/3.6/el7/zeroc-ice3.6.repo > \
