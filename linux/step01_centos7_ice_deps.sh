5e861101237015d35257b92c76f800c9e5e7e5cb
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-02-10 10:44:10 +0000

    Fix ice installation

--- linux/step01_centos7_ice_deps.sh
+++ linux/step01_centos7_ice_deps.sh
@@ -16,17 +16,28 @@ elif [ "$ICEVER" = "ice35-devel" ]; then
 
 	yum -y install ice ice-python-devel ice-java-devel ice-servers
 elif [ "$ICEVER" = "ice36" ]; then
-	#start-recommended
+   #start-recommended
     curl -sL https://zeroc.com/download/Ice/3.6/el7/zeroc-ice3.6.repo > \
     /etc/yum.repos.d/zeroc-ice3.6.repo
-	yum -y install gcc-c++
-	yum -y install libdb-utils
-	yum -y install openssl-devel bzip2-devel expat-devel
+    yum -y install gcc-c++
+    yum -y install libdb-utils
+    yum -y install openssl-devel bzip2-devel
 
-	yum -y install ice-all-runtime ice-all-devel
+    yum -y install ice-all-runtime
 
-	pip install "zeroc-ice>3.5,<3.7"
+    pip install "zeroc-ice>3.5,<3.7"
 	#end-recommended
 	# reset the locale
-	localedef -i en_US -f UTF-8 en_US.UTF-8
+    localedef -i en_US -f UTF-8 en_US.UTF-8
+elif [ "$ICEVER" = "ice36-devel" ]; then
+    curl -sL https://zeroc.com/download/Ice/3.6/el7/zeroc-ice3.6.repo > \
+    /etc/yum.repos.d/zeroc-ice3.6.repo
+    yum -y install gcc-c++
+    yum -y install libdb-utils
+    yum -y install openssl-devel bzip2-devel
+
+    yum -y install ice-all-runtime ice-all-devel
+
+    pip install "zeroc-ice>3.5,<3.7"
+    localedef -i en_US -f UTF-8 en_US.UTF-8
 fi
