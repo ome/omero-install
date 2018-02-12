da08a4d0b39707c84a33d38b9ebe9881e772ca47
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-02-12 11:48:04 +0000

    Pg configuration ubuntu

--- linux/step01_ubuntu1604_pg_deps.sh
+++ linux/step01_ubuntu1604_pg_deps.sh
@@ -7,6 +7,7 @@ PGVER=${PGVER:-pg94}
 if [ "$PGVER" = "pg95" ]; then
 	apt-get update
 	apt-get -y install postgresql
+	sed -i.bak -re 's/^(host.*)ident/\1md5/' /etc/postgresql/9.5/main/pg_hba.conf
 	service postgresql start
 elif [ "$PGVER" = "pg94" ]; then
 	apt-get -y install apt-transport-https
@@ -14,6 +15,7 @@ elif [ "$PGVER" = "pg94" ]; then
 	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 	apt-get update
 	apt-get -y install postgresql-9.4
+	sed -i.bak -re 's/^(host.*)ident/\1md5/' /etc/postgresql/9.4/main/pg_hba.conf
 	service postgresql start
 elif [ "$PGVER" = "pg96" ]; then
 	#start-recommended
@@ -22,6 +24,7 @@ elif [ "$PGVER" = "pg96" ]; then
 	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 	apt-get update
 	apt-get -y install postgresql-9.6
+	sed -i.bak -re 's/^(host.*)ident/\1md5/' /etc/postgresql/9.6/main/pg_hba.conf
 	service postgresql start
 	#end-recommended
 fi
