cb273c4933010d178e38ee684699d489ae4e8e70
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-08-06 14:16:35 +0100

    Cap the version of pytables

--- linux/step01_debian9_deps.sh
+++ linux/step01_debian9_deps.sh
@@ -8,7 +8,7 @@ apt-get -y install python-setuptools python-wheel virtualenv
 
 # python-tables will install tables version 3.3
 # but it does not work. install pytables from pypi.
-pip install tables
+pip install tables==3.4.4
 
 #start-web-dependencies
 apt-get -y install zlib1g-dev libjpeg-dev
