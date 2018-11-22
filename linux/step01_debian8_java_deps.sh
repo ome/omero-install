b276f5e6adfecc3f591322ffeec79a4427d42698
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-11-22 17:15:13 +0000

    Drop Java7 support

--- linux/step01_debian8_java_deps.sh
+++ linux/step01_debian8_java_deps.sh
@@ -2,16 +2,7 @@
 JAVAVER=${JAVAVER:-openjdk18}
 
 # install java
-if [ "$JAVAVER" = "openjdk17" ]; then
-	apt-get -y install openjdk-7-jre-headless
-elif [ "$JAVAVER" = "oracle17" ]; then
-	echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" > /etc/apt/sources.list.d/webupd8team-java.list
-	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list
-	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-	apt-get update
-	echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-	apt-get -y install oracle-java7-installer
-elif [ "$JAVAVER" = "oracle18" ]; then
+if [ "$JAVAVER" = "oracle18" ]; then
 	echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" > /etc/apt/sources.list.d/webupd8team-java.list
 	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
@@ -28,6 +19,4 @@ elif [ "$JAVAVER" = "openjdk18-devel" ]; then
 	echo 'deb http://httpredir.debian.org/debian jessie-backports main' > /etc/apt/sources.list.d/jessie-backports.list
 	apt-get update
 	apt-get -y install -t jessie-backports openjdk-8-jdk ca-certificates-java
-elif [ "$JAVAVER" = "openjdk17-devel" ]; then
-	apt-get -y install openjdk-7-jdk
 fi
--- linux/step01_ubuntu_java_deps.sh
+++ linux/step01_ubuntu_java_deps.sh
@@ -9,13 +9,6 @@ if [ "$JAVAVER" = "openjdk18" ]; then
 	apt-get update
 	apt-get -y install openjdk-8-jre
 	#end-recommended
-elif [ "$JAVAVER" = "oracle17" ]; then
-	apt-get -y install software-properties-common
-	add-apt-repository -y ppa:webupd8team/java
-	apt-get update
-	echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-	echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-	apt-get -y install oracle-java7-installer
 elif [ "$JAVAVER" = "oracle18" ]; then
 	apt-get -y install software-properties-common
 	add-apt-repository -y ppa:webupd8team/java
@@ -23,10 +16,6 @@ elif [ "$JAVAVER" = "oracle18" ]; then
 	echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 	echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 	apt-get -y install oracle-java8-installer
-elif [ "$JAVAVER" = "openjdk17" ]; then
-	apt-get -y install openjdk-7-jre-headless
-elif [ "$JAVAVER" = "openjdk17-devel" ]; then
-	apt-get -y install openjdk-7-jdk
 elif [ "$JAVAVER" = "openjdk18-devel" ]; then
 	apt-get -y install software-properties-common
 	add-apt-repository -y ppa:openjdk-r/ppa
