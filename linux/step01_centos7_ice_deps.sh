fd9cc7ffaf252707216d44c36bef385702ef3956
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-12-12 12:58:40 +0000

    Add support for ice 3.7

--- linux/step01_centos7_ice_deps.sh
+++ linux/step01_centos7_ice_deps.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ICEVER=${ICEVER:-ice36}
-
 # Ice installation
 if [ "$ICEVER" = "ice36" ]; then
    #start-recommended
@@ -27,5 +26,27 @@ elif [ "$ICEVER" = "ice36-devel" ]; then
     yum -y install ice-all-runtime ice-all-devel
 
     pip install "zeroc-ice>3.5,<3.7"
+    localedef -i en_US -f UTF-8 en_US.UTF-8
+elif [ "$ICEVER" = "ice37" ]; then
+    curl -sL https://zeroc.com/download/Ice/3.7/el7/zeroc-ice3.7.repo > \
+    /etc/yum.repos.d/zeroc-ice3.7.repo
+    yum -y install gcc-c++
+    yum -y install libdb-utils
+    yum -y install openssl-devel bzip2-devel
+
+    yum -y install ice-all-runtime
+
+    pip install "zeroc-ice>=3.7"
+    localedef -i en_US -f UTF-8 en_US.UTF-8
+elif [ "$ICEVER" = "ice37-devel" ]; then
+    curl -sL https://zeroc.com/download/Ice/3.7/el7/zeroc-ice3.7.repo > \
+    /etc/yum.repos.d/zeroc-ice3.7.repo
+    yum -y install gcc-c++
+    yum -y install libdb-utils
+    yum -y install openssl-devel bzip2-devel
+
+    yum -y install ice-all-runtime ice-all-devel
+
+    pip install "zeroc-ice>=3.7"
     localedef -i en_US -f UTF-8 en_US.UTF-8
 fi
