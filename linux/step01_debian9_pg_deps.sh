58a4ad5f66726809234e7b70d40ee2249875da89
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-12-12 20:48:31 +0000

    Add support for pg10

--- linux/step01_debian9_pg_deps.sh
+++ linux/step01_debian9_pg_deps.sh
@@ -2,8 +2,17 @@
 
 PGVER=${PGVER:-pg96}
 
-# Postgres installation: version installed will be 9.6
-#start-recommended
-apt-get -y install postgresql
-#end-recommended
-service postgresql start
+if [ "$PGVER" = "pg94" ]; then
+    # Postgres installation: version installed will be 9.6
+    #start-recommended
+    apt-get -y install postgresql
+    #end-recommended
+    service postgresql start
+elif [ "$PGVER" = "pg10" ]; then
+	apt-get install -y gnupg
+	echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+    apt-get update
+    apt-get install -y postgresql-10
+	service postgresql start
+fi
