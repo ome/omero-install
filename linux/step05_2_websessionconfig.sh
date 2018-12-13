8071de87f2f2744e12dfe6871770d6e2b40271b9
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-12-13 13:34:29 +0000

    Review usage of PY_ENV variable

--- linux/step05_2_websessionconfig.sh
+++ linux/step05_2_websessionconfig.sh
@@ -4,18 +4,7 @@ PY_ENV=${PY_ENV:-py27}
 
 cd ~omero
 
-echo "value=$PY_ENV"
 # Install required packages
-if [ "$PY_ENV" = "py27_scl" ]; then
-	set +u
-	source /opt/rh/python27/enable
-	set -u
-elif [ "$PY_ENV" = "py27_ius" ]; then
-	virtualenv -p /usr/bin/python2.7 /home/omero/omeroenv
-	set +u
-	source /home/omero/omeroenv/bin/activate
-	set -u
-fi
 
 # Register the app
 su - omero -c "OMERO.server/bin/omero config set omero.web.session_engine 'django.contrib.sessions.backends.cache'"
