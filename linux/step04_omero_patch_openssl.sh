3af4f6b836051674fccae8a888952e5f59c8e859
Simon Li <spli@dundee.ac.uk>
2019-05-17 11:54:21 +0100

    sed @SECLEVEL=0 clients.py not needed in 5.5
    See https://github.com/openmicroscopy/openmicroscopy/pull/6007

--- linux/step04_omero_patch_openssl.sh
+++ linux/step04_omero_patch_openssl.sh
@@ -2,6 +2,5 @@
 
 #start-seclevel
 OMERO.server/bin/omero config set omero.glacier2.IceSSL.Ciphers HIGH:ADH:@SECLEVEL=0
-sed -i 's/\("IceSSL.Ciphers".*ADH\)/\1:@SECLEVEL=0/' OMERO.server/lib/python/omero/clients.py
 #end-seclevel
 #clean files
