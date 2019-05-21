8113fbec9341080d0335679c49f3ca30395fbdbc
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-05-21 21:55:52 +0100

    build ice 3.6 from source

--- linux/step01_ubuntu1804_ice_deps.sh
+++ linux/step01_ubuntu1804_ice_deps.sh
@@ -24,37 +24,15 @@ if [ "$ICEVER" = "ice36" ]; then
         software-properties-common \
         zlib1g-dev
 
-    # Obtain BerekeleyDB and fix OpenSSL usage bug
-    wget https://zeroc.com/download/berkeley-db/db-5.3.28.NC.tar.gz
-    tar xzf db-5.3.28.NC.tar.gz
-    wget https://zeroc.com/download/berkeley-db/berkeley-db.5.3.28.patch
-    cd db-5.3.28.NC
-    sed -i '2896i#if OPENSSL_VERSION_NUMBER < 0x10100000L' src/repmgr/repmgr_net.c
-    sed -i '2906i#endif' src/repmgr/repmgr_net.c
-    patch -p0 < ../berkeley-db.5.3.28.patch
-    cd build_unix/
-    ../dist/configure --enable-cxx --enable-java --prefix=/usr
-    make
-    make install
-    # install mcpp version (maybe replace by package manager mcpp)
-    cd ../..
-    git clone https://github.com/zeroc-ice/mcpp.git
-    cd mcpp
-    make
-    cd ..
     wget https://github.com/zeroc-ice/ice/archive/v3.6.4.tar.gz
     tar xzvf v3.6.4.tar.gz
-    #git clone -b v3.6.4 https://github.com/zeroc-ice/ice.git
-    cd ice-3.6.4
-    sed -e "s/-Werror//g" -i ./cpp/config/Make.rules.Linux # Suppress -Werror flag (this may have uninteded consequences!!)
-    make cpp java
-    cd cpp 
-    make install
-    cd ../java
-    make install
-    echo /opt/Ice-3.6.4/lib > /etc/ld.so.conf.d/ice-x86_64.conf
-    ldconfig
+    cd ice-3.6.4/cpp
+    
+    make && make install
+
     pip install "zeroc-ice>3.5,<3.7"
+    echo /opt/Ice-3.6.4/lib/x86_64-linux-gnu > /etc/ld.so.conf.d/ice-x86_64.conf
+    ldconfig
     #end-recommended
 fi
 
