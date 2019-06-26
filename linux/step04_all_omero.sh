212c66868a453b7e583bfca0055432e4bf3a7e79
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-06-26 12:24:02 +0100

    remove check use for CentOS 6

--- linux/step04_all_omero.sh
+++ linux/step04_all_omero.sh
@@ -3,19 +3,10 @@
 set -e -u -x
 
 OMEROVER=${OMEROVER:-latest}
-PY_ENV=${PY_ENV:-py27}
 ICEVER=${ICEVER:-ice36}
 
 source `dirname $0`/settings.env
 #start-install
-
-if [ "$PY_ENV" = "py27" ]; then
-	#start-venv
-	virtualenv /home/omero/omeroenv
-	/home/omero/omeroenv/bin/pip install omego==0.6.0
-	#end-venv
-fi
-
 
 icevalue=3.6
 #start-install
@@ -33,6 +24,10 @@ fi
 # no server downloaded
 if [ ! -d OMERO.server* ]; then
 	# dev branches installed via omego
+	#start-venv
+	virtualenv /home/omero/omeroenv
+	/home/omero/omeroenv/bin/pip install omego==0.6.0
+	#end-venv
 	/home/omero/omeroenv/bin/omego download -q --ice $icevalue --branch $OMEROVER server
 fi
 
