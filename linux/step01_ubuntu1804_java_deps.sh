3b48e6652ea9212518fb49b3ce96e5f203098cf0
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-11-24 08:34:16 +0000

    remove oracle

--- linux/step01_ubuntu1804_java_deps.sh
+++ linux/step01_ubuntu1804_java_deps.sh
@@ -2,13 +2,7 @@
 JAVAVER=${JAVAVER:-openjdk1.8}
 
 # install java
-if [ "$JAVAVER" = "oracle11" ]; then
-    apt-get -y install software-properties-common
-    add-apt-repository ppa:linuxuprising/java
-    apt-get update -q
-    echo debconf shared/accepted-oracle-license-v1-2 select true | debconf-set-selections
-    apt-get install -y oracle-java11-installer
-elif [ "$JAVAVER" = "openjdk11-devel" ]; then
+if [ "$JAVAVER" = "openjdk11-devel" ]; then
     apt-get update -q
     apt-get install -y openjdk-11-jdk
 elif [ "$JAVAVER" = "openjdk1.8-devel" ]; then
