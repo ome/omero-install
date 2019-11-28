e7bfd720d246ed1f962e06a7fc91a4294ca2c7d7
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-11-28 15:31:12 +0000

    clean up dependencies

--- linux/step01_debian9_deps.sh
+++ linux/step01_debian9_deps.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-apt-get -y install python3-{pip,venv,yaml,jinja2}
+apt-get -y install python3-venv
 
 # to be installed if recommended/suggested is false
 apt-get -y install python3-setuptools python3-wheel
 
-
-# python-tables will install tables version 3.3
-# but it does not work. install pytables from pypi.
-pip3 install tables==3.4.4
-
 #start-web-dependencies
 apt-get -y install zlib1g-dev libjpeg-dev
-apt-get -y install python3-numpy
 #end-web-dependencies
