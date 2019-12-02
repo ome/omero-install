71fff538ba1cfe8d11b3cb0ed68bf6b1c87a17fe
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-12-02 14:15:48 +0000

    add char cut by error

--- linux/step01_ubuntu1804_pg_deps.sh
+++ linux/step01_ubuntu1804_pg_deps.sh
@@ -10,7 +10,7 @@ if [ "$PGVER" = "pg10" ]; then
 elif [ "$PGVER" = "pg11" ]; then
     #start-recommended
     echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-    get --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
     apt-get update
     apt-get -y install postgresql
     service postgresql start
