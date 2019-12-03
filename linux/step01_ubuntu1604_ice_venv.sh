cfeb267c83be3283fc51b2c0eda5142478d49675
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-12-03 22:36:59 +0000

    rename venv to match ansible

--- linux/step01_ubuntu1604_ice_venv.sh
+++ linux/step01_ubuntu1604_ice_venv.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-VENV_SERVER=${VENV_SERVER:-/opt/omero/server/venv}
+VENV_SERVER=${VENV_SERVER:-/opt/omero/server/venv3}
 #start-ice-py
 # Create a virtual env and activate it
 python3 -mvenv $VENV_SERVER
@@ -11,7 +11,7 @@ python3 -mvenv $VENV_SERVER
 $VENV_SERVER/bin/pip install https://github.com/ome/zeroc-ice-py-ubuntu1604/releases/download/0.2.0/zeroc_ice-3.6.5-cp35-cp35m-linux_x86_64.whl
 #end-ice-py
 
-VENV_WEB=${VENV_WEB:-/opt/omero/web/venv}
+VENV_WEB=${VENV_WEB:-/opt/omero/web/venv3}
 python3 -mvenv $VENV_WEB
 
 $VENV_WEB/bin/pip install https://github.com/ome/zeroc-ice-py-ubuntu1604/releases/download/0.2.0/zeroc_ice-3.6.5-cp35-cp35m-linux_x86_64.whl
