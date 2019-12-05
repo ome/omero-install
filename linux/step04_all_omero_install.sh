033b1f767e6748df897552144686053fd72e9737
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-12-05 11:51:18 +0000

    change url

--- linux/step04_all_omero_install.sh
+++ linux/step04_all_omero_install.sh
@@ -19,8 +19,7 @@ if [ "$ICEVER" = "ice36" ]; then
 	if [ $OMEROVER == "latest" ]; then
 		#start-release-ice36
 		cd /opt/omero/server
-		#SERVER=https://downloads.openmicroscopy.org/latest/omero5.6/server-ice36.zip
-		SERVER=https://downloads.openmicroscopy.org/omero/5.6.0-m2/artifacts/OMERO.server-5.6.0-m2-ice36-b126.zip
+		SERVER=https://downloads.openmicroscopy.org/latest/omero/5.6/server-ice36.zip
 		wget -q $SERVER -O OMERO.server-ice36.zip
 		unzip -q OMERO.server*
 		#end-release-ice36
