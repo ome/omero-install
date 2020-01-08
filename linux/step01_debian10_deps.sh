26b50bbb164c887ac7d774870028649ff69be84a
Dominik Lindner <d.lindner@dundee.ac.uk>
2020-01-08 16:22:10 +0000

    Fix openssl issues by commenting out default options

--- linux/step01_debian10_deps.sh
+++ linux/step01_debian10_deps.sh
@@ -3,3 +3,7 @@
 apt-get -y install\
     python3 \
     python3-venv
+
+# Fix openssl issues
+sed -e '/MinProtocol/ s/^#*/#/' -i /etc/ssl/openssl.cnf
+sed -e '/CipherString/ s/^#*/#/' -i /etc/ssl/openssl.cnf
