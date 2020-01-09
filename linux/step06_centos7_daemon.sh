cd9bf1c74e48211201b22cef8b43901c2fff3c88
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-01-09 14:04:06 +0000

    fix name

--- linux/step06_centos7_daemon.sh
+++ linux/step06_centos7_daemon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #start-recommended
-cp omero-systemd.service /etc/systemd/system/omero.service
+cp omero-server-systemd.service /etc/systemd/system/omero-server.service
 if [ ! "${container:-}" = docker ]; then
     systemctl daemon-reload
 fi
-systemctl enable omero.service
+systemctl enable omero-server.service
 #end-recommended
