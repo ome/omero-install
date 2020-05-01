d674d282c0c9fb27f033ff02d3ce921bd6b8de81
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-05-01 16:44:27 +0100

    generate centos8 doc

--- linux/autogenerate.sh
+++ linux/autogenerate.sh
@@ -14,7 +14,7 @@ echo "${l}"
 
 #generate the walkthrough for all supported os
 function generate_all() {
-	values=(centos7 ubuntu1604 debian9 debian10 ubuntu1804 ubuntu2004)
+	values=(centos7 centos8 ubuntu1604 debian9 debian10 ubuntu1804 ubuntu2004)
 	for os in "${values[@]}"; do
   		echo "${os}"
   		 generate ${os}
@@ -36,6 +36,8 @@ EOF
 N=$OS
 if [[ $OS =~ "debian" ]] || [[ $OS =~ "ubuntu" ]] ; then
 	N="ubuntu"
+elif [[ $OS =~ "centos" ]]  ; then
+    N="centos7"
 fi
 echo -en '\n' >> $file
 echo "#start-step01: As root, install dependencies" >> $file
@@ -68,6 +70,8 @@ echo "# install dependencies" >> $file
 N=$OS
 if [[ $OS =~ "ubuntu" ]] ; then
 	N="ubuntu1604"
+elif [[ $OS =~ "centos" ]] ; then
+    N="centos7"
 fi
 line=$(sed -n '2,$p' $dir/step01_"$N"_deps.sh)
 echo "$line" >> $file
@@ -106,7 +110,7 @@ fi
 N=$OS
 echo -en '\n' >> $file
 echo "# install Postgres" >> $file
-if [ $OS = "centos7" ] ; then
+if [[ $OS =~ "centos" ]] ; then
     number=$(sed -n '/#start-postgresql-installation-general/=' $dir/step01_"$N"_pg_deps.sh)
     nrs=$((number+1))
     number=$(sed -n '/#end-postgresql-installation-general/=' $dir/step01_"$N"_pg_deps.sh)
@@ -225,7 +229,7 @@ line=$(sed -n ''$ns',$p' $dir/setup_omero_db.sh)
 echo "$line" >> $file
 echo "#end-step04" >> $file
 
-if [[ $OS =~ "debian" ]] || [[ $OS != "ubuntu1604" ]]; then
+if [[ $OS != "centos7" ]] && [[ $OS != "ubuntu1604" ]]; then
 	echo "#start-patch-openssl" >> $file
 	number=$(sed -n '/#start-seclevel/=' $dir/step04_omero_patch_openssl.sh)
 	ns=$((number))
