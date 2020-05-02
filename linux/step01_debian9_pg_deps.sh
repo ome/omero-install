a993c95c6aa99bf5c3a58538e39d660fe1273ce9
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-05-02 09:07:58 +0100

    add support for pg12

--- linux/step01_debian9_pg_deps.sh
+++ linux/step01_debian9_pg_deps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PGVER=${PGVER:-pg11}
+
 if [[ "$PGVER" =~ ^(pg94|pg95)$ ]]; then
     PGVER="pg11"
 fi
@@ -23,6 +24,15 @@ elif [ "$PGVER" = "pg11" ]; then
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
     apt-get update
     apt-get install -y postgresql-11
+    service postgresql start
+    #end-recommended
+elif [ "$PGVER" = "pg12" ]; then
+    #start-recommended
+    apt-get install -y gnupg
+    echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+    apt-get update
+    apt-get install -y postgresql-12
     service postgresql start
     #end-recommended
 fi
--- linux/step01_ubuntu1804_pg_deps.sh
+++ linux/step01_ubuntu1804_pg_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PGVER=${PGVER:-pg11}
-
+PGVER=pg12
 #Postgres 10
 if [ "$PGVER" = "pg10" ]; then
     apt-get update
@@ -14,6 +14,15 @@ elif [ "$PGVER" = "pg11" ]; then
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
     apt-get update
     apt-get -y install postgresql-11
+    service postgresql start
+    #end-recommended
+elif [ "$PGVER" = "pg12" ]; then
+    #start-recommended
+    apt-get install -y gnupg
+    echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+    apt-get update
+    apt-get -y install postgresql-12
     service postgresql start
     #end-recommended
 fi
