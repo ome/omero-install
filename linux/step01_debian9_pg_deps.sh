dd28ac6451823b43db4319958d859e8db676e453
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-05-02 09:24:50 +0100

    remove duplicated markers

--- linux/step01_debian9_pg_deps.sh
+++ linux/step01_debian9_pg_deps.sh
@@ -27,12 +27,10 @@ elif [ "$PGVER" = "pg11" ]; then
     service postgresql start
     #end-recommended
 elif [ "$PGVER" = "pg12" ]; then
-    #start-recommended
     apt-get install -y gnupg
     echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" > /etc/apt/sources.list.d/pgdg.list
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
     apt-get update
     apt-get install -y postgresql-12
     service postgresql start
-    #end-recommended
 fi
--- linux/step01_ubuntu1804_pg_deps.sh
+++ linux/step01_ubuntu1804_pg_deps.sh
@@ -17,12 +17,10 @@ elif [ "$PGVER" = "pg11" ]; then
     service postgresql start
     #end-recommended
 elif [ "$PGVER" = "pg12" ]; then
-    #start-recommended
     apt-get install -y gnupg
     echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/pgdg.list
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
     apt-get update
     apt-get -y install postgresql-12
     service postgresql start
-    #end-recommended
 fi
