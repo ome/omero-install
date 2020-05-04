ccc6ce806d2fcedd67d9feda3c0408d08daa20f7
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-05-04 14:21:43 +0100

    install git branch

--- linux/step01_debian10_deps.sh
+++ linux/step01_debian10_deps.sh
@@ -2,7 +2,8 @@
 
 apt-get -y install\
     python3 \
-    python3-venv
+    python3-venv \
+    git
 
 # Fix openssl issues
 sed -e '/MinProtocol/ s/^#*/#/' -i /etc/ssl/openssl.cnf
--- linux/step01_ubuntu1604_deps.sh
+++ linux/step01_ubuntu1604_deps.sh
@@ -4,4 +4,5 @@ apt-get -y install \
 	unzip \
 	wget \
 	python3 \
-	python3-venv
+	python3-venv \
+    git
