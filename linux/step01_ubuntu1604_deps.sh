11db097d29d554274996ac169a84765a20b5ba20
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-05-12 17:03:57 +0100

    add python 3.6 option for ubuntu

--- linux/step01_ubuntu1604_deps.sh
+++ linux/step01_ubuntu1604_deps.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+PYTHONVER=${PYTHONVER:-default}
+ICEVER=${ICEVER:-ice36}
+
+# start-add-dependencies
 apt-get update
 apt-get -y install \
 	unzip \
-	wget \
-	python3 \
-	python3-venv
+	wget
+# end-add-dependencies
+if [ "$PYTHONVER" = "py36" ]; then
+    # start-install-Python36
+    add-apt-repository ppa:deadsnakes/ppa
+    apt-get update
+    apt-get install -y python3.6 python3.6-venv python3.6-dev
+    # dependencies required to install Ice Python binding
+    apt-get install -y build-essential libssl-dev libffi-dev \
+                       libbz2-dev libxml2-dev libxslt1-dev zlib1g-dev
+    # end-install-Python36                   
+else
+    # start-default
+    apt-get -y install python3 python3-venv
+    # end-default
+fi
--- linux/step01_ubuntu1604_ice_venv.sh
+++ linux/step01_ubuntu1604_ice_venv.sh
@@ -2,13 +2,25 @@
 
 set -x
 
+PYTHONVER==${PYTHONVER:-default}
 VENV_SERVER=${VENV_SERVER:-/opt/omero/server/venv3}
 #start-ice-py
-# Create a virtual env and activate it
-python3 -mvenv $VENV_SERVER
 
-# Install the Ice Python binding
-$VENV_SERVER/bin/pip install https://github.com/ome/zeroc-ice-py-ubuntu1604/releases/download/0.2.0/zeroc_ice-3.6.5-cp35-cp35m-linux_x86_64.whl
+if [ "$PYTHONVER" = "py36" ]; then
+    # start-py36
+    # Create a virtual env and activate it
+    python3.6 -mvenv $VENV_SERVER
+    $VENV_SERVER/bin/pip install zeroc-ice==3.6.5
+    # end-py36
+else
+    # start-default
+    # Create a virtual env and activate it
+    python3 -mvenv $VENV_SERVER
+
+    # Install the Ice Python binding
+    $VENV_SERVER/bin/pip install https://github.com/ome/zeroc-ice-py-ubuntu1604/releases/download/0.2.0/zeroc_ice-3.6.5-cp35-cp35m-linux_x86_64.whl
+    # end-default
+fi
 
 # Install server dependencies
 $VENV_SERVER/bin/pip install omero-server[ubuntu1604]
