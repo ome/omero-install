d3e2aeef7ab74e1c85fdc5900b2007e2e131c3ab
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-11-11 18:07:36 +0000

    remove it flag

--- linux/test/test_services.sh
+++ linux/test/test_services.sh
@@ -11,13 +11,13 @@ CNAME=omeroinstall_$ENV
 SETTINGS=${SETTINGS:-/home/omero-server/settings.env}
 
 # start docker container
-docker run -dit --name $CNAME -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /run -p 8080:80 omero_install_test_$ENV
+docker run -d --name $CNAME -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /run -p 8080:80 omero_install_test_$ENV
 
 # check if container is running
 docker inspect -f {{.State.Running}} $CNAME
 
 # wait for omero to start up and accept connections
-docker exec -it $CNAME /bin/bash -c 'd=10; \
+docker exec $CNAME /bin/bash -c 'd=10; \
     until [ -f /opt/omero/server/OMERO.server/var/log/Blitz-0.log ]; \
         do \
             sleep 10; \
@@ -28,7 +28,7 @@ docker exec -it $CNAME /bin/bash -c 'd=10; \
         done; \
     echo "File found"; exit'
 
-docker exec -it $CNAME /bin/bash -c 'd=10; \
+docker exec $CNAME /bin/bash -c 'd=10; \
     while ! grep "OMERO.blitz now accepting connections" /opt/omero/server/OMERO.server/var/log/Blitz-0.log ; \
         do \
             sleep 20; \
@@ -40,17 +40,17 @@ docker exec -it $CNAME /bin/bash -c 'd=10; \
     echo "Entry found"; exit'
 
 #check OMERO.server service status
-docker exec -it $CNAME /bin/bash -c "service omero-server status -l --no-pager"
+docker exec $CNAME /bin/bash -c "service omero-server status -l --no-pager"
 
-docker exec -it $CNAME /bin/bash -c "su - omero-server -c \". ${SETTINGS} && omero admin diagnostics\""
+docker exec $CNAME /bin/bash -c "su - omero-server -c \". ${SETTINGS} && omero admin diagnostics\""
 
 # Log in to OMERO.server
-docker exec -it $CNAME /bin/bash -c "su - omero-server -c \". ${SETTINGS} && omero login -s localhost -p 4064 -u root -w ${OMERO_ROOT_PASS}\""
+docker exec $CNAME /bin/bash -c "su - omero-server -c \". ${SETTINGS} && omero login -s localhost -p 4064 -u root -w ${OMERO_ROOT_PASS}\""
 
-docker exec -it $CNAME /bin/bash -c "su - omero-server -c \". ${SETTINGS} && touch test_image.fake && omero import test_image.fake\""
+docker exec $CNAME /bin/bash -c "su - omero-server -c \". ${SETTINGS} && touch test_image.fake && omero import test_image.fake\""
 
 # Log out
-docker exec -it $CNAME /bin/bash -c "su - omero-server -c \". ${SETTINGS} && omero logout\""
+docker exec $CNAME /bin/bash -c "su - omero-server -c \". ${SETTINGS} && omero logout\""
 
 # stop and cleanup
 docker stop $CNAME
