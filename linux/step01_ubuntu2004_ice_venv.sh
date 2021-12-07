5cbb818d25a97407cd64d5517d96a90251e5a4d2
Jean-Marie Burel <j.burel@dundee.ac.uk>
2021-12-07 11:21:10 +0000

    upgrade pip

--- linux/step01_ubuntu2004_ice_venv.sh
+++ linux/step01_ubuntu2004_ice_venv.sh
@@ -4,8 +4,11 @@ set -e -u -x
 
 VENV_SERVER=${VENV_SERVER:-/opt/omero/server/venv3}
 #start-ice-py
-# Create a virtual env and activate it
+# Create a virtual env
 python3 -mvenv $VENV_SERVER
+
+# Upgrade pip
+$VENV_SERVER/bin/pip install --upgrade pip
 
 # Install the Ice Python binding
 $VENV_SERVER/bin/pip install https://github.com/ome/zeroc-ice-ubuntu2004/releases/download/0.2.0/zeroc_ice-3.6.5-cp38-cp38-linux_x86_64.whl
