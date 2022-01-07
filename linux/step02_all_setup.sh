8e798aa7d757a58291c924267a277ea2155accfd
Jean-Marie Burel <j.burel@dundee.ac.uk>
2022-01-07 09:56:04 +0000

    give ownership to conda envs to omero-server user

--- linux/step02_all_setup.sh
+++ linux/step02_all_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$(getent passwd omero)" ]; then
-	#start-create-user
+    #start-create-user
     useradd -mr omero-server
     # Give a password to the omero user
     # e.g. passwd omero-server
@@ -12,3 +12,4 @@ chmod a+X ~omero-server
 
 mkdir -p "$OMERO_DATA_DIR"
 chown omero-server "$OMERO_DATA_DIR"
+chown -R omero-server "$CONDA_ENV"
