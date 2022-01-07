ab804736bea0b9ef6cf1a792991b0967fc0b91c6
Jean-Marie Burel <j.burel@dundee.ac.uk>
2022-01-07 09:55:17 +0000

    create dir

--- linux/step04_all_omero_install.sh
+++ linux/step04_all_omero_install.sh
@@ -9,15 +9,16 @@ set -eux
 
 #start-install
 if [ "$ICEVER" = "ice36" ]; then
-	if [ $OMEROVER == "latest" ]; then
-		#start-release-ice36
-		cd /opt/omero/server
-		SERVER=https://github.com/ome/openmicroscopy/releases/download/v5.6.3/OMERO.server-5.6.3-ice36-b228.zip
-		wget -q $SERVER -O OMERO.server-ice36.zip
-		unzip -q OMERO.server*
-		#end-release-ice36
-		rm OMERO.server-ice36.zip
-	fi
+    if [ $OMEROVER == "latest" ]; then
+        #start-release-ice36
+        mkdir -p /opt/omero/server
+        cd /opt/omero/server
+        SERVER=https://github.com/ome/openmicroscopy/releases/download/v5.6.3/OMERO.server-5.6.3-ice36-b228.zip
+        wget -q $SERVER -O OMERO.server-ice36.zip
+        unzip -q OMERO.server*
+        #end-release-ice36
+        rm OMERO.server-ice36.zip
+    fi
 fi
 
 #start-link
