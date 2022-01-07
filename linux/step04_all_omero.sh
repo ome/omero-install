94609ad7c415c0be582ce314ca73f851195591af
Jean-Marie Burel <j.burel@dundee.ac.uk>
2022-01-07 09:57:55 +0000

    install and configure

--- linux/step04_all_omero.sh
+++ linux/step04_all_omero.sh
@@ -4,17 +4,26 @@ set -x
 
 . `dirname $0`/settings.env
 
+# Create the conda environment
+$CONDA_ENV/bin/conda create --name venv3 python=3.9 pip
+
+# Install omero-py
+$CONDA_ENV/bin/conda install -n venv3 -c ome omero-py
+
+# Install server dependencies
+$CONDA_ENV/envs/venv3/bin/pip install omero-server[default]
+
 #configure
-omero config set omero.data.dir "$OMERO_DATA_DIR"
-omero config set omero.db.name "$OMERO_DB_NAME"
-omero config set omero.db.user "$OMERO_DB_USER"
-omero config set omero.db.pass "$OMERO_DB_PASS"
+$CONDA_ENV/envs/venv3/bin/omero config set omero.data.dir "$OMERO_DATA_DIR"
+$CONDA_ENV/envs/venv3/bin/omero config set omero.db.name "$OMERO_DB_NAME"
+$CONDA_ENV/envs/venv3/bin/omero config set omero.db.user "$OMERO_DB_USER"
+$CONDA_ENV/envs/venv3/bin/omero config set omero.db.pass "$OMERO_DB_PASS"
 #start-db
 
 #start-deb-latest
-omero db script -f $OMERODIR/db.sql --password "$OMERO_ROOT_PASS"
+$CONDA_ENV/envs/venv3/bin/omero db script -f $OMERODIR/db.sql --password "$OMERO_ROOT_PASS"
 #end-deb-latest
 
 #start-seclevel
-omero certificates
+$CONDA_ENV/envs/venv3/bin/omero certificates
 #end-seclevel
