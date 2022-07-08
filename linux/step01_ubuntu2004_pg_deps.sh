41717c1d5be97440838a825ddec14febffb2111f
Jean-Marie Burel <j.burel@dundee.ac.uk>
2022-07-08 12:20:57 +0100

    install pg14

--- linux/step01_ubuntu2004_pg_deps.sh
+++ linux/step01_ubuntu2004_pg_deps.sh
@@ -3,9 +3,18 @@
 PGVER=${PGVER:-pg12}
 
 #Postgres 12
-#start-recommended
-apt-get update
-apt-get -y install postgresql
-service postgresql start
-#end-recommended
+if [ "$PGVER" = "pg12" ]; then
+    #start-recommended
+    apt-get update
+    apt-get -y install postgresql
+    service postgresql start
+    #end-recommended
+elif [ "$PGVER" = "pg14" ]; then
+	apt-get install -y gnupg
+    echo "deb http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+    apt-get update
+    apt-get -y install postgresql-14
+    service postgresql start
+fi
 
