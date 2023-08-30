fedcd45f766dd9de29772b1e1bb4cda13ac3c229
Jean-Marie Burel <j.burel@dundee.ac.uk>
2023-08-30 15:07:23 +0100

    use official wheel

--- linux/step01_rocky9_ice_venv.sh
+++ linux/step01_rocky9_ice_venv.sh
@@ -11,8 +11,7 @@ python3 -mvenv $VENV_SERVER
 $VENV_SERVER/bin/pip install --upgrade pip
 
 # Install the Ice Python binding
-$VENV_SERVER/bin/pip install https://github.com/sbesson/zeroc-ice-py-rockylinux9-x86_64/releases/download/20230719/zeroc_ice-3.6.5-cp39-cp39-linux_x86_64.whl
-
+$VENV_SERVER/bin/pip install https://github.com/glencoesoftware/zeroc-ice-py-rhel9-x86_64/releases/download/20230830/zeroc_ice-3.6.5-cp39-cp39-linux_x86_64.whl
 # Install server dependencies
 $VENV_SERVER/bin/pip install omero-server
 #end-ice-py
