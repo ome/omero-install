a1cbd90d48b464bd67a7a000a4917a7e208e2504
Jean-Marie Burel <j.burel@dundee.ac.uk>
2023-09-07 09:18:07 +0100

    use new tar

--- linux/step01_rocky9_ice_deps.sh
+++ linux/step01_rocky9_ice_deps.sh
@@ -5,8 +5,8 @@ dnf config-manager --set-enabled crb
 yum -y install bzip2 expat libdb-cxx
 
 cd /tmp
-wget https://github.com/sbesson/zeroc-ice-rockylinux9-x86_64/releases/download/202307018/Ice-3.6.5-rockylinux9-x86_64.tar.gz
-tar xf Ice-3.6.5-rockylinux9-x86_64.tar.gz
+wget https://github.com/sbesson/zeroc-ice-rhel9-x86_64/releases/download/20230830/Ice-3.6.5-rhel9-x86_64.tar.gz
+tar xf Ice-3.6.5-rhel9-x86_64.tar.gz
 mv Ice-3.6.5 /opt/ice-3.6.5
 echo /opt/ice-3.6.5/lib64 > /etc/ld.so.conf.d/ice-x86_64.conf
 ldconfig
