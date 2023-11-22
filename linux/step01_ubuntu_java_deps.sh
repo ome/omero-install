53e96eca1d4b433f1c3d29e686bb74703af5aaf3
Jean-Marie Burel <j.burel@dundee.ac.uk>
2023-11-22 14:43:59 +0000

    update to match step01_ubuntu18.04_java_deps.sh

--- linux/step01_ubuntu_java_deps.sh
+++ linux/step01_ubuntu_java_deps.sh
@@ -2,25 +2,17 @@
 JAVAVER=${JAVAVER:-openjdk11}
 # install java
 if [ "$JAVAVER" = "openjdk1.8" ]; then
-    apt-get -y install software-properties-common
-    add-apt-repository -y ppa:openjdk-r/ppa
     apt-get update
     apt-get -y install openjdk-8-jre
 elif [ "$JAVAVER" = "openjdk1.8-devel" ]; then
-    apt-get -y install software-properties-common
-    add-apt-repository -y ppa:openjdk-r/ppa
     apt-get update
     apt-get -y install openjdk-8-jdk
 elif [ "$JAVAVER" = "openjdk11-devel" ]; then
-    apt-get -y install software-properties-common
-    add-apt-repository ppa:openjdk-r/ppa
     apt-get update -q
     apt-get install -y openjdk-11-jdk
 elif [ "$JAVAVER" = "openjdk11" ]; then
     #start-recommended
-    apt-get -y install software-properties-common
-    add-apt-repository ppa:openjdk-r/ppa
     apt-get update -q
-    apt-get install -y openjdk-11-jre
+    apt-get install -y openjdk-11-jdk
     #end-recommended
 fi
