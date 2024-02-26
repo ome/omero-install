df463df3af9a6a377f048fcf38bec47eb05f6927
Jean-Marie Burel <j.burel@dundee.ac.uk>
2024-02-26 11:44:13 +0000

    make psql15 the recommended version for ubuntu 22.04

--- linux/step01_ubuntu2204_pg_deps.sh
+++ linux/step01_ubuntu2204_pg_deps.sh
@@ -3,18 +3,18 @@
 PGVER=${PGVER:-pg15}
 
 if [ "$PGVER" = "pg14" ]; then
-  #start-recommended
   apt-get update
   apt-get -y install postgresql
   service postgresql start
-  #end-recommended
 elif [ "$PGVER" = "pg15" ]; then
+  #start-recommended
   apt-get install -y gnupg
   echo "deb http://apt.postgresql.org/pub/repos/apt jammy-pgdg main" > /etc/apt/sources.list.d/pgdg.list
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
   apt-get update
   apt-get -y install postgresql-15
   service postgresql start
+    #end-recommended
 elif [ "$PGVER" = "pg16" ]; then
   apt-get install -y gnupg
   echo "deb http://apt.postgresql.org/pub/repos/apt jammy-pgdg main" > /etc/apt/sources.list.d/pgdg.list
