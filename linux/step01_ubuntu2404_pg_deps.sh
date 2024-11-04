1b42c86dbe02a7e987445f00550b89ac3418f67a
Jean-Marie Burel <j.burel@dundee.ac.uk>
2024-11-04 19:20:50 +0000

    Add installation instruction for Ubuntu 24.04

---
+++ linux/step01_ubuntu2404_pg_deps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PGVER=${PGVER:-pg15}
+
+if [ "$PGVER" = "pg14" ]; then
+  apt-get update
+  apt-get -y install postgresql
+  service postgresql start
+elif [ "$PGVER" = "pg15" ]; then
+  #start-recommended
+  apt-get install -y gnupg
+  echo "deb http://apt.postgresql.org/pub/repos/apt noble-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+  apt-get update
+  apt-get -y install postgresql-15
+  service postgresql start
+    #end-recommended
+elif [ "$PGVER" = "pg16" ]; then
+  apt-get install -y gnupg
+  echo "deb http://apt.postgresql.org/pub/repos/apt noble-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+  apt-get update
+  apt-get -y install postgresql-16
+  service postgresql start
+fi
