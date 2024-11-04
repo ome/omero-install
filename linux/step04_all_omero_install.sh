b6a059e3f3a8635f10cd82734e94823b2e53b2bf
Jean-Marie Burel <j.burel@dundee.ac.uk>
2024-11-04 19:03:16 +0000

    Change location where to download the file from

--- linux/step04_all_omero_install.sh
+++ linux/step04_all_omero_install.sh
@@ -8,9 +8,11 @@ set -eux
 
 #start-install
 if [ $OMEROVER == "latest" ]; then
+	# version number and ice version to be dropped
 	#start-release-ice36
 	cd /opt/omero/server
-	SERVER=https://downloads.openmicroscopy.org/omero/5.6/server-ice36.zip
+	SERVER=https://github.com/ome/openmicroscopy/releases/latest/download/OMERO.server-5.6.13-ice36.zip
+
 	wget -q $SERVER -O OMERO.server-ice36.zip
 	unzip -q OMERO.server*
 	#end-release-ice36
