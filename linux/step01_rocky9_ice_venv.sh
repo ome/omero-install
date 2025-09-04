e87725574cdccc1ac892bf5be9880403e6456711
Dominik Lindner <d.lindner@dundee.ac.uk>
2025-09-04 11:50:26 +0100

    fix typo

--- linux/step01_rocky9_ice_venv.sh
+++ linux/step01_rocky9_ice_venv.sh
@@ -11,6 +11,7 @@ python3.12 -mvenv $VENV_SERVER
 $VENV_SERVER/bin/pip install --upgrade pip
 
 # Install the Ice Python binding
-$VENV_SERVER/bin/pip install https://github.com/glencoesoftware/zeroc-ice-py-linux-x86_64/releases/download/20240202/zeroc_ice-3.6.5-cp312-cp312-manylinux_2_28_x86_64.whl# Install server dependencies
+$VENV_SERVER/bin/pip install https://github.com/glencoesoftware/zeroc-ice-py-linux-x86_64/releases/download/20240202/zeroc_ice-3.6.5-cp312-cp312-manylinux_2_28_x86_64.whl
+# Install server dependencies
 $VENV_SERVER/bin/pip install omero-server
 #end-ice-py
