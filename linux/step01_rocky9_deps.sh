ae072f28d4f749c7d82be0000040fb41b8ac73ca
Dominik Lindner <d.lindner@dundee.ac.uk>
2025-09-04 10:50:57 +0100

    Use Python3.12

--- linux/step01_rocky9_deps.sh
+++ linux/step01_rocky9_deps.sh
@@ -4,7 +4,7 @@ PGVER=${PGVER:-pg15}
 JAVAVER=${JAVAVER:-openjdk11}
 # General additional packages installation
 #start-general
-dnf -y install python3 unzip bzip2 wget bc openssl
+dnf -y install python3.12 unzip bzip2 tar wget bc openssl
 #end-general
 
 # Java installation
